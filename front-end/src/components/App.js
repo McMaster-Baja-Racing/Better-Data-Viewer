23fffb9a7dc61a18d079e25229c1c8b6f2beda90
Kai <83799062+gr812b@users.noreply.github.com>
2022-11-21 16:17:52 -0500

    Chart work V0.0.1

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,21 +1,25 @@
 import '../styles/App.css';
+// import useState
+import React, { useState } from 'react';
+import Chart from './Chart';
 
 function App() {
+
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        {count}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Chart/>
+
+        <button onClick={() => {
+          console.log("Button was clicked");
+          setCount(count + 1);
+        }}>Change to Apple</button>
       </header>
     </div>
   );
---
+++ front-end/src/components/Chart.js
@@ -0,0 +1,158 @@
+import '../styles/chart.css'
+import React, { useState, useEffect } from 'react';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+// import f gps speed
+import Papa from "papaparse";
+
+const Chart = () => {
+    const [chartOptions, setChartOptions] = useState({
+        chart: {
+            zoomType: 'x',
+            width: 1000,
+        },
+        title: {
+            text: 'Forward GPS Speed'
+        },
+        subtitle: {
+            text: document.ontouchstart === undefined ?
+                'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+        },
+        xAxis: {
+            type: 'time'
+        },
+        yAxis: {
+            title: {
+                text: 'Speed (km/h)'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        }
+    });
+
+    const [parsedData, setParsedData] = useState([]);
+
+    const changeHandler = (event) => {
+        // Passing file data (event.target.files[0]) to parse using Papa.parse
+        Papa.parse(event.target.files[0], {
+            header: true,
+            skipEmptyLines: true,
+            complete: function (results) {
+                setParsedData(results.data);
+            },
+        });
+    };
+
+    useEffect(() => {
+        console.log(parsedData);
+        // Format the data to be used in the chart (2D array), the format being an array of objects with a key and value
+        var formattedData = [];
+        for (var i = 0; i < parsedData.length; i++) {
+            formattedData.push([parseInt(parsedData[i]['Timestamp (ms)']), parseFloat(parsedData[i]['F_GPS_SPEED'])]);
+        }
+        // Update the chart options with the new data
+        setChartOptions({
+            chart: {
+                zoomType: 'x',
+                width: 1000,
+            },
+            title: {
+                text: 'Forward GPS Speed'
+            },
+            subtitle: {
+                text: document.ontouchstart === undefined ?
+                    'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+            },
+            xAxis: {
+                type: 'time'
+            },
+            yAxis: {
+                title: {
+                    text: 'Speed (km/h)'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+            series: [
+                { data: formattedData }
+            ]
+        })
+    }, [parsedData]);
+
+    return (
+        <div>
+            <div>
+                {/* File Uploader */}
+                <input
+                    type="file"
+                    name="file"
+                    accept=".csv"
+                    onChange={changeHandler}
+                    style={{ display: "block", margin: "10px auto" }}
+                />
+            </div>
+            <div className='chart'>
+                <HighchartsReact
+                    highcharts={Highcharts}
+                    options={chartOptions}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Chart;
