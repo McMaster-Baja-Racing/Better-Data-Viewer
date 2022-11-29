840950865789cab593a9fc5308a5979372d8b435
Kai <83799062+gr812b@users.noreply.github.com>
2022-11-29 16:51:46 -0500

    api backend

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,10 +1,30 @@
 import '../styles/App.css';
 // import useState
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Chart from './Chart';
 import Topbar from './Topbar';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    callBackendAPI()
+      .then(res => setData(res.express))
+      .catch(err => console.log(err));
+  }, []);
+
+  // fetching the GET route from the Express server which matches the GET route from server.js
+  const callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    console.log(body);
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
 
   return (
     <><Topbar />
@@ -13,6 +33,7 @@ function App() {
       <header className="App-header">
       <Chart />
       </header>
+      <p className="App-intro">{data}</p>
     </div></>
   );
 }
