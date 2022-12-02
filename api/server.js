9324702cb8e42f2bce7e931658b0c54a81e5efd5
Kai <83799062+gr812b@users.noreply.github.com>
2022-12-02 17:49:54 -0500

    I forgor, prob some backend

--- api/server.js
+++ api/server.js
@@ -4,8 +4,7 @@ const express = require('express');
 const app = express(); 
 const port = process.env.PORT || 5000; 
 
-//import local java modules, like "../dataanalyzer/AccelCurveAnalyzer.java"
-const {AccelCurveAnalyzer} = require('./dataanalyzer/AccelCurveAnalyzer.java');
+const exec = require('child_process').exec
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
@@ -17,5 +16,14 @@ app.get('/express_backend', (req, res) => {
 
 //
 app.get('/bananas', (req, res) => {
-    res.send({ csv: 'bananas' });
+    const child = exec('X:\Code\Projects\Baja\Better-Data-Viewer\storage\csv\CSVReader.class', (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            res.send({error: err, status: 500, errorOutput: stderr})
+            return
+        }
+        // it is important to have json structure in your output or you need to create a logic which parse the output
+        res.send(stdout)
+      })
+
 });
