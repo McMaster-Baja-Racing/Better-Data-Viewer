5351fa1cdcf46a8cdb666c60670c249257a41853
Travis <wingt@mcmaster.ca>
2022-12-02 17:41:45 -0500

    front end updates

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,20 +1,29 @@
 import '../styles/App.css';
+import '../styles/styles.css';
 // import useState
+
+import { Modal } from "./Modal";
 import React, { useState } from 'react';
 import Chart from './Chart';
 import Topbar from './Topbar';
 
+
 function App() {
+  const [showModal, setShowModal] = useState(false);
+const openModal = () => {
+    setShowModal(true);
+  };
 
   return (
     <><Topbar />
     <div className="App">
-      
-      <header className="App-header">
+    <header className="App-header">
+      <button onClick={openModal}>Create Graph</button>
+      {showModal ? <Modal setShowModal={setShowModal} /> : null}
       <Chart />
-      </header>
-    </div></>
+    </header>
+      </div>
+      </>
   );
 }
-
 export default App;
