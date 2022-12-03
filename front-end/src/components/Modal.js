d1db9bda547ebfe70430298104e211b6069e1685
t-wing11 <Traviswing20@gmail.com>
2022-12-03 13:15:56 -0500

    drop down menu

--- front-end/src/components/Modal.js
+++ front-end/src/components/Modal.js
@@ -3,6 +3,7 @@
 import { useRef } from "react";
 import ReactDom from "react-dom";
 import '../styles/styles.css';
+import Dropdown from "./Dropdown";
 import { useState, useEffect } from 'react';
 
 export const Modal = ({ setShowModal }) => {
@@ -11,7 +12,11 @@ export const Modal = ({ setShowModal }) => {
   const modalRef = useRef();
 
   const changeHandler = (event) => { console.log(event.target.value); };
-
+  const options = [
+    {value: '1', label: 'one'}, 
+    {value: '2', label: 'two'}, 
+    {value: '3', label: 'three'}
+  ];
   const closeModal = (e) => {
     if (e.target === modalRef.current) {
       setShowModal(false);
@@ -22,22 +27,18 @@ export const Modal = ({ setShowModal }) => {
     <div className="container" ref={modalRef} onClick={closeModal}>
       <div className="modal">
         <div classname="small">
-          <input
+        <input
             type="file"
             name="file"
             accept=".csv"
             onChange={changeHandler}
             style={{ display: "block", margin: "10px auto" }}
           />
-        </div>
-        <div classname="small">
-          <input
-            type="file"
-            name="file"
-            accept=".csv"
-            onChange={changeHandler}
-            style={{ display: "block", margin: "10px auto" }}
-          />
+          <Dropdown
+        placeHolder="Select # Dimensions"
+        options={options}
+        onChange={(value) => console.log(value)}
+      />
         </div>
         <button onClick={() => setShowModal(false)}>X</button>
       </div>
