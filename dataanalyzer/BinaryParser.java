e28f1f3d07bf56a82edc8a04d4191ceb851b642b
Gavin <carrog1@mcmaster.ca>
2022-12-17 23:48:44 -0500

    fixed bug

---
+++ dataanalyzer/BinaryParser.java
@@ -0,0 +1,10 @@
+/*package dataanalyzer;
+
+public class BinaryParser {
+    public static native void to_csv(String fileName);
+
+    static {
+        System.loadLibrary("binary-to-csv-lib");
+    }
+
+}*/
