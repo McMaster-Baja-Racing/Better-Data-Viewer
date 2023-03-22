c2369ea984ac11f2ec8b0fc44716e8a3a9dff1aa
Kai <83799062+gr812b@users.noreply.github.com>
2023-03-22 19:08:37 -0400

    Sawlma fuckwery sewtup W2

--- front-end/src/components/Help.js
+++ front-end/src/components/Help.js
@@ -1,7 +1,17 @@
+import "../styles/help.css"
+
 const Help = () => {
+
+
+    function myFunction() {
+        var popup = document.getElementById("myPopup");
+        popup.classList.toggle("show");
+    }
+
+
     return (
-        <div>
-        <h1>Help</h1>
+        <div class="popup" onclick="myFunction()">Click me!
+            <span class="popuptext" id="myPopup">Popup text...</span>
         </div>
     );
 }
