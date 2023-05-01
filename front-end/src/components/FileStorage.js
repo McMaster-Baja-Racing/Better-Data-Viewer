4f18c4b0e21156952b786bb3c69610c2b1213675
Kai <83799062+gr812b@users.noreply.github.com>
2023-05-01 01:09:19 -0400

    File selection working as expected
    This code boring ah hell - but it works so maybe keep it

--- front-end/src/components/FileStorage.js
+++ front-end/src/components/FileStorage.js
@@ -1,32 +1,59 @@
+import 'font-awesome/css/font-awesome.min.css';
 import '../styles/fileStorage.css'
 import 'react-keyed-file-browser/dist/react-keyed-file-browser.css';
 import FileBrowser, {Icons} from 'react-keyed-file-browser';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import { useState } from 'react'
 
+
+
 const CustomFileRenderer = (props) => {
-    const { isSelected, onClick, browserProps } = props;
+    const { selectedFiles, setSelectedFiles, browserProps } = props;
+
     const file = {
         key: props.fileKey,
         name: props.name,
         size: props.size,
         modified: props.modified,
         extension: props.name.split('.').pop(),
+        depth: props.depth,
     };
 
-    console.log(props)
+    //console.log(props)
+    const handleSelectFile = (e) => {
+        console.log(file)
+        const fileIndex = selectedFiles.findIndex((selectedFile) => selectedFile.key === file.key);
+
+        if (fileIndex === -1) {
+            // Add the file to the selectedFiles array if it's not already there
+            console.log([...selectedFiles, file])
+            setSelectedFiles([...selectedFiles, file]);
+        } else {
+            // Remove the file from the selectedFiles array if it's already there
+            console.log(selectedFiles.filter((_, index) => index !== fileIndex))
+            setSelectedFiles(selectedFiles.filter((_, index) => index !== fileIndex));
+        }
+    };
+
+    const isSelected = selectedFiles.some((selectedFile) => selectedFile.key === file.key);
+
+    const paddingLeft = 16; // This is the base padding value in pixels.
+    const depthPadding = paddingLeft * file.depth + 12;
 
     const { connectDragSource, connectDropTarget } = browserProps;
 
     return (
         // ${isSelected ? 'selected' : ''}
-        <tr {...connectDragSource} {...connectDropTarget} className={`file`} onClick={onClick}>
-            <td className="name">{file.name}</td>
+        <tr {...connectDragSource} {...connectDropTarget} className={`file ${isSelected ? 'selected' : ''}`} onClick={handleSelectFile}>
+            <td className="name" style={{ paddingLeft: depthPadding }}>{file.name}</td>
             <td className="size">{file.size / (1024 * 1024) + " MB"}</td>
             <td className="modified">{file.modified ? file.modified : "-"}</td>
         </tr>
+        
     );
 };
-
+ 
 const FileStorage = () => {
 
     const [browseFiles, setBrowseFiles] = useState([
@@ -47,22 +74,6 @@ const FileStorage = () => {
 
     const [selectedFiles, setSelectedFiles] = useState([]);
 
-    const handleSelectFile = (file) => {
-        const fileIndex = selectedFiles.findIndex((selectedFile) => selectedFile.key === file.key);
-
-        if (fileIndex === -1) {
-            // Add the file to the selectedFiles array if it's not already there
-            console.log([...selectedFiles, file])
-            setSelectedFiles([...selectedFiles, file]);
-
-        } else {
-            // Remove the file from the selectedFiles array if it's already there
-            console.log(selectedFiles.filter((_, index) => index !== fileIndex))
-            setSelectedFiles(selectedFiles.filter((_, index) => index !== fileIndex));
-        }
-
-    };
-
     return (
         <div>
             <h1>File Storage</h1>
@@ -71,8 +82,7 @@ const FileStorage = () => {
                 <FileBrowser
                     files={browseFiles}
                     icons={Icons.FontAwesome(4)}
-
-                    // fileRendererProps={{ handleSelectFile }}
+                    fileRendererProps={{ selectedFiles, setSelectedFiles }}
                     fileRenderer={CustomFileRenderer}
                 />
             </div>
