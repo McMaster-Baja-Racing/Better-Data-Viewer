42cabd0737f545ca0f6b242b487db335a5398d85
Kai <83799062+gr812b@users.noreply.github.com>
2023-09-28 19:46:11 -0400

    Some styling for the dropdowns and first page modal

--- front-end/src/components/GraphSettings.js
+++ front-end/src/components/GraphSettings.js
@@ -32,28 +32,30 @@ const GraphSettings = ({ setDisplayPage, setFiles, setGraphType, setLiveCheck })
       <h1>Welcome to the Data Viewer!</h1>
       <h3>Instructions</h3>
       <ol>
-        <li>Select the type of graph you want</li>
-        <li>Select the files you want to graph</li>
-        <li>Select the column headers you want on your graph</li>
-        <li>Select the analyzer you want to use on your data</li>
+        <li>Select the type of graph you want.</li>
+        <li>Select the files you want to graph.</li>
+        <li>Select the columns of your graph you want.</li>
+        <li>Select the analyzer you want to use on your data.</li>
+        <li>For more, repeat steps 3 & 4, clicking "Add Series" in between.</li>
       </ol>
       <h3>Graph Options</h3>
-      <div className='graphOptionsCol1'>
-        <div className='liveDataBox'>
-          <div className="textboxbold">Live Data</div><br></br>
-          <input type="checkbox" id="liveDataCheckbox" name="liveData" value="true"></input>
-        </div>
-        <br/>
+      <div className='graphOptions'>
         <div className="graphTypesBox">
-          <div className="textboxbold">Graph Types</div>
+          <div className="graphOptionsText">Graph Types</div>
           <div className="GraphTypeSelect">
             <select id="graphTypeSelect" >
-              <option value="line">line</option>
-              <option value="spline">spline</option>
-              <option value="scatter"> scatter</option>
+              <option value="line">Line</option>
+              <option value="spline">Spline</option>
+              <option value="scatter">Scatter</option>
             </select>
           </div>
         </div>
+        <div className='liveDataBox'>
+          <div className="graphOptionsText">Live Data</div>
+          <input type="checkbox" id="liveDataCheckbox" name="liveData" value="true"></input>
+        </div>
+        
+        
       </div>
 
       <button className="PageButton" onClick={() => { liveData(); graphType(); listFiles(); setDisplayPage(2); }}>Next</button>
