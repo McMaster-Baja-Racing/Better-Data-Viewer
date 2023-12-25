179e3ac99c93866298479483c09d8a098b7cb045
Kai <83799062+gr812b@users.noreply.github.com>
2023-12-24 21:11:14 -0500

    Working rotations + more
    Ready for quaternion data, just need to collect a bit

--- front-end/src/components/ModelViewer.js
+++ front-end/src/components/ModelViewer.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useRef } from 'react'
+import React, { useRef, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stage } from '@react-three/drei'
 import { Eevee } from './Eevee.js'
@@ -7,20 +7,23 @@ import '../styles/modelViewer.css'
 const ModelViewer = () => {
 
     const ref = useRef()
+
     return (
         <div className="modelContainer">
-            <Canvas size={[`2000px`,`3000px`]} shadows dpr={[1, 2]} camera={{ fov: 20 }}>
+            <Canvas size={[`2000px`, `3000px`]} shadows dpr={[1, 2]} camera={{ fov: 40, position: [40, 0, 0] }}>
                 <Suspense fallback={null}>
-                    <Stage controls={ref} preset="rembrandt" intensity={1} environment="city">
+                    <Stage controls={ref} preset="rembrandt" intensity={1} environment="lobby">
+                    <directionalLight position={[-5, 10, -35]} intensity={2.0}  />
+                    <directionalLight position={[5, 10, 5]} intensity={2.0}  />
+                    <ambientLight intensity={0.3} />
                         false
                         <Eevee />
                         false
                     </Stage>
                 </Suspense>
-                <OrbitControls ref={ref} autoRotate />
+                <OrbitControls ref={ref}  /> {/* autoRotate */}
             </Canvas>
         </div>
-
     )
 }
 
