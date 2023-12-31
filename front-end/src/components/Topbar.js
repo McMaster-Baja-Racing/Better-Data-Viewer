75c445bedf4759e2575dde28927f817683744e88
Jack Walmsley <jackawalmsley@gmail.com>
2023-12-31 01:06:19 -0500

    Added other graph icon

--- front-end/src/components/Topbar.js
+++ front-end/src/components/Topbar.js
@@ -38,7 +38,7 @@ const Topbar = ({ setModal }) => {
                         : <img className="icon" src={process.env.PUBLIC_URL + 'icons/liveOff.svg'} alt="Live Mode Off" />}
                 </button>
                 <button title="Create Graph" className="createGraph" onClick={() => setModal('Create')}>
-                    <img className="icon" src={process.env.PUBLIC_URL + 'icons/newGraph.svg'} alt="Create Graph" />
+                    <img className="icon" src={process.env.PUBLIC_URL + 'icons/newGraph2.svg'} alt="Create Graph" />
                 </button>
                 <button title="Upload Files" className="uploadFiles" onClick={() => setModal('Upload')}>
                 <img className="icon"src={process.env.PUBLIC_URL + 'icons/upload.svg'} alt="Upload" />
