0c5ba7ce7874c79b630706f0ba2aa50a45ca3320
Jack Walmsley <jackawalmsley@gmail.com>
2023-12-30 19:11:54 -0500

    Pretty white icons in topbar

--- front-end/src/components/Topbar.js
+++ front-end/src/components/Topbar.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 const Topbar = ({ setModal }) => {
 
-    const [liveStatus, setLiveStatus] = useState("Begin Live Data");
+    const [liveStatus, setLiveStatus] = useState(false);
     //This function notifies the backend to begin listening on a certain port for live data
     var formData = new FormData();
     formData.append("port", "COM2");
@@ -17,10 +17,10 @@ const Topbar = ({ setModal }) => {
             console.log(err);
         });
 
-        if (liveStatus === "Begin Live Data") {
-            setLiveStatus("Stop Live Data");
+        if (liveStatus === false) {
+            setLiveStatus(true);
         } else {
-            setLiveStatus("Begin Live Data");
+            setLiveStatus(true);
         }
     }
 
@@ -32,11 +32,23 @@ const Topbar = ({ setModal }) => {
                 <img src={process.env.PUBLIC_URL + 'eeee.gif'} alt="baja_logo"/>
             </div>
             <div className="buttons">
-                <button className="beginLive" onClick={beginLiveData}>{liveStatus}</button>
-                <button className="createGraph" onClick={() => setModal('Create')}>Create Graph</button>
-                <button className="uploadFiles" onClick={() => setModal('Upload')}>Upload Files</button>
-                <button className="downloadFiles" onClick={() => setModal('Download')}>Download Files</button>
-                <button className="helpModal" onClick={() => setModal('Help')}>Help</button>
+                <button className="beginLive" onClick={beginLiveData}>
+                    {liveStatus
+                        ? <img className="icon" src={process.env.PUBLIC_URL + 'icons/liveOn.svg'} alt="Live Mode On" />
+                        : <img className="icon" src={process.env.PUBLIC_URL + 'icons/liveOff.svg'} alt="Live Mode Off" />}
+                </button>
+                <button className="createGraph" onClick={() => setModal('Create')}>
+                    <img className="icon" src={process.env.PUBLIC_URL + 'icons/add.svg'} alt="Create Graph" />
+                </button>
+                <button className="uploadFiles" onClick={() => setModal('Upload')}>
+                <img className="icon"src={process.env.PUBLIC_URL + 'icons/cloudUpload.svg'} alt="Upload" />
+                </button>
+                <button className="downloadFiles" onClick={() => setModal('Download')}>
+                <img className="icon"src={process.env.PUBLIC_URL + 'icons/cloudDownload.svg'} alt="Download" />
+                </button>
+                <button className="helpModal" onClick={() => setModal('Help')}>
+                    <img className="icon"src={process.env.PUBLIC_URL + 'icons/help.svg'} alt="Help" />
+                </button>
             </div>
         </div>
     );
