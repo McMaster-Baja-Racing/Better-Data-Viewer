19900d26932b6f161573bd3a5a419953d008d135
Jack Walmsley <jackawalmsley@gmail.com>
2024-01-22 21:11:47 -0500

    hmmm

--- front-end/src/components/map/MapDisplay.js
+++ front-end/src/components/map/MapDisplay.js
@@ -2,21 +2,34 @@ import { MapContainer, TileLayer, Marker, Popup, Rectangle, GeoJSON } from 'reac
 import 'leaflet/dist/leaflet.css';
 
 const MapDisplay = ({ bounds, zoom }) => {
-    let testLine = {"type": "LineString","coordinates": [[43.2614, -79.93], [43.262, -79.932]]}
+    // let testLine = {"type": "LineString","coordinates": [[43.2614, -79.93], [43.262, -79.932]]}
+    let testLine = {
+        "type": "Feature",
+        "properties": {
+            "name": "Coors Field",
+            "amenity": "Baseball Stadium",
+            "popupContent": "This is where the Rockies play!"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [43.262, -79.932]
+        }
+    };
     return (
         <div id="mapBackground">
             <MapContainer bounds={bounds} zoom={zoom} zoomControl={false} scrollWheelZoom={false} dragging={false} style={{height: "100%"}}>
-            <Rectangle bounds={bounds}></Rectangle>
-            <GeoJSON attribution="pog swag" data={testLine}></GeoJSON>
+            {/* <Rectangle bounds={bounds}></Rectangle> */}
+            
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
+            <Marker position={[43.2614, -79.93]}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
             </Marker>
+            <GeoJSON key={testLine} attribution="pog swag" data={testLine}></GeoJSON>
         </MapContainer>
         </div>
 
