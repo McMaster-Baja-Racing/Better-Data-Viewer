4ce9cd15d1338e460fca7724f751918d608ed79d
Kai <83799062+gr812b@users.noreply.github.com>
2024-01-23 23:06:39 -0500

    Multiple charts layout

--- front-end/src/components/Charts.js
+++ front-end/src/components/Charts.js
@@ -5,7 +5,13 @@ const Charts = ({ chartInformation }) => {
 
     return (
         <div className="charts">
+            <div className="bigChart">
+                <Chart chartInformation={chartInformation} />
+            </div>
             <Chart chartInformation={chartInformation} />
+            <Chart chartInformation={chartInformation} />
+            <Chart chartInformation={chartInformation} />
+
         </div>
     )
 }
