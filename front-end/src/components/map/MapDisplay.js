81d7bfec56d933ff50d28a12baaf2f9a7d936656
Jack Walmsley <jackawalmsley@gmail.com>
2024-01-25 19:05:37 -0500

    Test path showing properly

--- front-end/src/components/map/MapDisplay.js
+++ front-end/src/components/map/MapDisplay.js
@@ -1,30 +1,36 @@
 import { MapContainer, TileLayer, Marker, Popup, Rectangle, GeoJSON } from 'react-leaflet';
+import L from "leaflet";
 import 'leaflet/dist/leaflet.css';
-// let coords = require('./testData.json')
+let coords = require('./testData.json')
 
 const MapDisplay = ({ bounds, zoom }) => {
-    // let testLine = {"type": "LineString","coordinates": [[43.2614, -79.93], [43.262, -79.932]]}
     // GEOJSON USES LONG, LAT NOT LAT, LONG
-    let testLine = {
-        type: "Feature",
-        "properties": {
-            "name": "Coors Field",
-            "amenity": "Baseball Stadium",
-            "popupContent": "This is where the Rockies play!"
-        },
-        geometry: {
+    let testLine = [{
             type: "LineString",
-            coordinates: []
-        }
-    };
+        coordinates: coords
+    },
+        // {
+        //     type: "Point",
+        //     coordinates: coords[0]
+        // },
+        // {
+        //     type: "Point",
+        //     coordinates: coords[coords.length - 1]
+        // }
+    ];
+
+    const marker = L.icon({ iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png", shadowUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-shadow.png",     iconAnchor:   [13, 38]})
     return (
         <div id="mapBackground">
-            <MapContainer bounds={bounds} style={{height: "100%"}} dragging={true} scrollWheelZoom={true}>
-            {/* <GeoJSON key={testLine} data={testLine} style={{stroke: true, color: "#ff00ff", weight: 1}}></GeoJSON> */}
+            <MapContainer bounds={bounds} style={{ height: "100%" }} dragging={true} scrollWheelZoom={true} >
+                <GeoJSON key={testLine} data={testLine} style={{ stroke: true, color: "#ff2222", weight: 3 }}></GeoJSON>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    maxZoom={20}
             />
+                <Marker position={[coords[0][1], coords[0][0]]} icon={marker} />
+                <Marker position={[coords[coords.length-1][1], coords[coords.length-1][0]]} icon={marker} />
         </MapContainer>
         </div>
 
