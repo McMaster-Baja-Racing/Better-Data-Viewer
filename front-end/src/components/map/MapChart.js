f0be69bdc814932f4419d2e74800e34695e0d0ce
Jack Walmsley <jackawalmsley@gmail.com>
2024-02-09 01:36:24 -0500

    Tool selection

--- front-end/src/components/map/MapChart.js
+++ front-end/src/components/map/MapChart.js
@@ -1,12 +1,24 @@
-import Chart from '../Chart';
+import { useState } from 'react';
+import '../../styles/mapChart.css';
 import MapDisplay from './MapDisplay';
-import '../../styles/mapChart.css'
+import ToolSelection from './ToolSelection';
+let coords = require('./testData.json')
 
-const MapChart = ({ chartInformation, bounds, zoom }) => {
-    console.log(chartInformation)
-    return(
-        <div id={"mapChart"} style={{top: "0", left: "0"}}>
-            <MapDisplay bounds={bounds} zoom={zoom}/>
+const tools = ["Start",  "End", "Checkpoint"]
+
+const MapChart = () => {
+    const [currTool, setCurrTool] = useState(tools[0]);
+    function changeTool(index) {
+        setCurrTool(tools[index]);
+    }
+    return (
+        <div style={{ width: "100%", height: "100%", display: "flex", flexDirection: "column"}}>
+            <div id={"ui"} style={{display: "block", flex: "0 0", margin:"8px", textAlign: "left"}}>
+                <ToolSelection options={tools} onChange={changeTool}> </ToolSelection>
+            </div>
+            <div style={{flex: "1 0"}}>
+                <MapDisplay coords={coords} toolType={currTool}/>
+            </div>
         </div>
     )
 }
---
+++ front-end/src/components/map/ToolSelection.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const ToolSelection = ({ options, onChange }) => {
+    const [selected, setSelected] = useState(0);
+
+    function handleClick(index) {
+        setSelected(index);
+        onChange(index);
+    }
+    return (
+        options.map((elem, index) => <button key={elem} className={'map_ui_button ' + (selected === index ? 'selected' : '')} onClick={() => handleClick(index)}> {elem} </button>)
+    )   
+}
+
+export default ToolSelection;
