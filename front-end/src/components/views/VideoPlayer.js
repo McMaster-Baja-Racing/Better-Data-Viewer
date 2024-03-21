48a0eca6c0d113c7671b8139786994bddf71fd01
Kai Arseneau <kaiarseneau@hotmail.com>
2024-03-21 19:25:50 -0400

    Added react player + fixed sizing

--- front-end/src/components/views/VideoPlayer.js
+++ front-end/src/components/views/VideoPlayer.js
@@ -1,14 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
 import '../../styles/videoPlayerStyles.css';
 import { getDuration } from '../../lib/videoUtils';
+import ReactPlayer from 'react-player';
 
 const VideoPlayer = ({ video, videoTimestamp, setVideoTimestamp }) => {
 
   const [videoURL, setVideoURL] = useState('');
-  const videoRef = useRef(null);
-  const [currentTime, setCurrentTime] = useState(0);
-  const duration = getDuration(video) / 1000;
-  const [prevState, setPrevState] = useState();
 
   useEffect(() => {
     // Fetch data when the component mounts
@@ -24,53 +21,13 @@ const VideoPlayer = ({ video, videoTimestamp, setVideoTimestamp }) => {
       });
   }, []); // Empty dependency array ensures that the fetch is only performed once
 
-  const togglePlay = () => {
-    if (videoRef.current.paused) {
-      videoRef.current.play();
-    } else {
-      videoRef.current.pause();
-    }
-  };
-
-  const formatTime = (seconds) => {  // the time display, converting past time to minutes
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.floor(seconds % 60);
-    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
-  };
-
-  const updateTimestamp = () => {
-    const time = videoRef.current.currentTime;
-    setCurrentTime(time);
-    setVideoTimestamp(time*1000)
-  }
-  
-  const seek = (event) => {
-    const seekTime = event.target.value;
-    videoRef.current.currentTime = seekTime;
-  };
-
-  const seekDown = () => {
-    setPrevState(videoRef.current.paused);
-    videoRef.current.pause();
-  }
-
-  const seekUp = () => {
-    if (!prevState) {
-      videoRef.current.play();
-    }
-  }
-
   return (
     <div className = "background">
         <div className = "pageWrap">
           <div className = "pageContainer">
             <div className = "videoContainerBox">
-              <video src={videoURL} ref={videoRef} onTimeUpdate={updateTimestamp} onClick={togglePlay} className = "center" id="video"/>
+              <ReactPlayer url={videoURL} onProgress={(e) => {setVideoTimestamp(e.playedSeconds*1000)}} progressInterval={1} className="center" id="video" controls/>
             </div>
-            <div className="timeDisplay">
-              {formatTime(currentTime)} / {formatTime(duration)}
-            </div>
-            <input type="range" step={"0.1"} min="0" max={duration} value={currentTime} onChange={seek} onMouseDown={seekDown} onMouseUp={seekUp} className="seekbar" />
           </div>
         </div>
     </div>
