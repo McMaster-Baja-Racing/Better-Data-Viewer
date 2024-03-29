1aa339f4866b1d53881d4cc610d98404e1289bef
Kai Arseneau <kaiarseneau@hotmail.com>
2024-03-29 13:31:50 -0400

    Parser, live and model linting

--- API/src/main/java/backend/API/readwrite/Writer.java
+++ API/src/main/java/backend/API/readwrite/Writer.java
@@ -3,19 +3,19 @@ package backend.API.readwrite;
 import java.util.List;
 
 public abstract class Writer {
-    protected String filepath;
+  protected String filepath;
 
-    public Writer(String filepath) {
-        this.filepath = filepath;
-    }
+  public Writer(String filepath) {
+    this.filepath = filepath;
+  }
 
-    public abstract void write(List<List<String>> data);
+  public abstract void write(List<List<String>> data);
 
-    public String getFilepath() {
-        return filepath;
-    }
+  public String getFilepath() {
+    return filepath;
+  }
 
-    public void setFilepath(String filepath) {
-        this.filepath = filepath;
-    }
+  public void setFilepath(String filepath) {
+    this.filepath = filepath;
+  }
 }
