fae69ed671b4308aebc517c635d2c4868c241912
Kai Arseneau <kaiarseneau@hotmail.com>
2024-03-30 20:01:42 -0400

    Linting js lib

--- front-end/src/lib/viewUtils.js
+++ front-end/src/lib/viewUtils.js
@@ -1,15 +1,15 @@
 // Replaces the view at the given index with the new view
 export const replaceViewAtIndex = (viewInformation, index, newView) => {
-    return viewInformation.map((view, i) => {
-        if (i === index) {
-            return newView;
-        }
-        return view;
-    })
-}
+  return viewInformation.map((view, i) => {
+    if (i === index) {
+      return newView;
+    }
+    return view;
+  });
+};
 
 // Adds a new view at the given index shifting all other views to the right and then trimming the last view
 export const insertViewAtIndex = (viewInformation, index, newView) => {
-    const tempViewInformation = viewInformation.slice(0, index).concat(newView).concat(viewInformation.slice(index, viewInformation.length - 1))
-    return tempViewInformation.slice(0, viewInformation.length)
-}
+  const tempViewInformation = viewInformation.slice(0, index).concat(newView).concat(viewInformation.slice(index, viewInformation.length - 1));
+  return tempViewInformation.slice(0, viewInformation.length);
+};
