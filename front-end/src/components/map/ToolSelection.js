4c189bed6dfb2a022e718036fdaa45140852f48f
Kai Arseneau <kaiarseneau@hotmail.com>
2024-03-30 21:27:42 -0400

    Linting map

--- front-end/src/components/map/ToolSelection.js
+++ front-end/src/components/map/ToolSelection.js
@@ -1,13 +1,15 @@
+import React from 'react';
+
 const ToolSelection = ({ options, selected, setSelected }) => {
 
-    function handleClick(index) {
-        setSelected(index);
-    }
-    return (
-        <span>
-            {options.map((elem, index) => <button key={elem} className={'map_ui_tool map_ui_button ' + (selected === index ? 'selected' : '')} onClick={() => handleClick(index)}> {elem} </button>)}
-        </span>
-    )
-}
+  function handleClick(index) {
+    setSelected(index);
+  }
+  return (
+    <span>
+      {options.map((elem, index) => <button key={elem} className={'map_ui_tool map_ui_button ' + (selected === index ? 'selected' : '')} onClick={() => handleClick(index)}> {elem} </button>)}
+    </span>
+  );
+};
 
 export default ToolSelection;
