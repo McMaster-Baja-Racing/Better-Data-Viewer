53163f2330ae0105d26d0f0e1891c244251029df
Kai Arseneau <kaiarseneau@hotmail.com>
2024-04-10 19:36:08 -0400

    Grr

--- API/src/main/java/backend/API/FileUploadController.java
+++ API/src/main/java/backend/API/FileUploadController.java
@@ -238,8 +238,9 @@ public class FileUploadController {
                   // Get the path and filename of each file and print it
                   LocalDateTime[] timespan =
                       storageService.getTimespan(path.toString(), (LocalDateTime) container[1]);
-                  timespans.add(new fileTimespan(
-                      path.toString().replace("\\", "/"), timespan[0], timespan[1]));
+                  timespans.add(
+                      new fileTimespan(
+                          path.toString().replace("\\", "/"), timespan[0], timespan[1]));
                 }
               }
             });
