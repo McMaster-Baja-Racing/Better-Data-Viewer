93e3b6081608dab70ed73889d72a7a37e398a201
Kai Arseneau <kaiarseneau@hotmail.com>
2024-04-20 16:42:58 -0400

    Refactored Average Analyzer
    Ensured it uses the new reader/writer, which cleans up the code considerably

--- backend/src/main/java/com/mcmasterbaja/analyzer/AverageAnalyzer.java
+++ backend/src/main/java/com/mcmasterbaja/analyzer/AverageAnalyzer.java
@@ -1,11 +1,12 @@
 package com.mcmasterbaja.analyzer;
 
-import com.mcmasterbaja.readwrite.CSVReader;
-import com.mcmasterbaja.readwrite.CSVWriter;
-import com.mcmasterbaja.readwrite.Reader;
-import com.mcmasterbaja.readwrite.Writer;
-import java.util.ArrayList;
+
+import java.io.IOException;
 import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.ICSVWriter;
+import com.opencsv.exceptions.CsvException;
 
 public class AverageAnalyzer extends Analyzer {
   // This class takes the average of a range of a column and returns it as a double
@@ -18,7 +19,7 @@ public class AverageAnalyzer extends Analyzer {
     // index
   }
 
-  public void analyze() {
+  public void analyze() throws IOException, CsvException {
     System.out.println(
         "Taking the average of "
             + super.inputFiles[0]
@@ -29,56 +30,41 @@ public class AverageAnalyzer extends Analyzer {
             + " to "
             + range[1]);
 
-    Reader r = new CSVReader(super.inputFiles[0]);
-    Writer w = new CSVWriter(super.outputFiles[0]);
+    CSVReader reader = getReader(super.inputFiles[0]);
+    ICSVWriter writer = getWriter(super.outputFiles[0]);
 
-    // Create a single data point with the average
-    List<String> dataPoint = new ArrayList<String>(2);
-    dataPoint.add("Temp");
-    dataPoint.add("Average");
-
-    // Add header
-    List<List<String>> dataPoints = new ArrayList<List<String>>();
-    dataPoints.add(dataPoint);
-
-    // Reset
-    dataPoint = new ArrayList<String>(2);
-
-    // Add the average to the dataPoint
-    dataPoint.add(Double.toString(0));
-    dataPoint.add(Double.toString(average(r.read(), range[0], range[1])));
-
-    // Add the dataPoint to the dataPoints
-    dataPoints.add(dataPoint);
-
-    w.write(dataPoints);
+    String[] headers = {"TempColumn", "Average"};
+    writer.writeNext(headers);
+    
+    String[] dataPoint = {"0", Double.toString(average(reader.readAll(), range[0], range[1]))};
+    writer.writeNext(dataPoint);
   }
 
   // Takes average at found indices of second column
-  public double average(List<List<String>> data, int start, int end) {
+  public double average(List<String[]> data, int start, int end) {
     int startIndex = binarySearch(data, start);
     int endIndex = binarySearch(data, end);
 
     double sum = 0;
     for (int i = startIndex; i <= endIndex; i++) {
-      sum += Double.parseDouble(data.get(i).get(1));
+      sum += Double.parseDouble(data.get(i)[1]);
     }
     return sum / (endIndex - startIndex + 1);
   }
 
   // Binary search finds index relative to first column. It should find the closest value
-  public int binarySearch(List<List<String>> data, int target) {
+  public int binarySearch(List<String[]> data, int target) {
     int low = 0;
     int high = data.size() - 1;
     int mid = (low + high) / 2;
 
     while (low <= high) {
       mid = (low + high) / 2;
-      if (Integer.parseInt(data.get(mid).get(0)) == target) {
+      if (Integer.parseInt(data.get(mid)[0]) == target) {
         return mid;
-      } else if (Integer.parseInt(data.get(mid).get(0)) < target) {
+      } else if (Integer.parseInt(data.get(mid)[0]) < target) {
         low = mid + 1;
-      } else if (Integer.parseInt(data.get(mid).get(0)) > target) {
+      } else if (Integer.parseInt(data.get(mid)[0]) > target) {
         high = mid - 1;
       }
     }
@@ -87,10 +73,15 @@ public class AverageAnalyzer extends Analyzer {
 
   // Make a main to test it
   public static void main(String[] args) {
-    String[] inputFiles = {"X:/Code/Projects/Baja/Better-Data-Viewer/data/temp.csv"};
-    String[] outputFiles = {"X:/Code/Projects/Baja/Better-Data-Viewer/data/temp2.csv"};
-    int[] range = {51, 75};
+    String[] inputFiles = {"/Users/kaiarseneau/Documents/GitHub/Better-Data-Viewer/backend/uploads/csv/000628 - WFT/BATT PERC.csv"};
+    String[] outputFiles = {"/Users/kaiarseneau/Documents/GitHub/Better-Data-Viewer/backend/uploads/csv/000628 - WFT/temp.csv"};
+    int[] range = {60000, 210000};
     AverageAnalyzer a = new AverageAnalyzer(inputFiles, outputFiles, range);
-    a.analyze();
+    try {
+      a.analyze();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
   }
 }
