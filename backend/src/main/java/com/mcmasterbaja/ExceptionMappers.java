00ec621493bca43797d0bafe2c11d413a87a9c80
Kai Arseneau <kaiarseneau@hotmail.com>
2024-04-30 20:25:48 -0700

    Added error handling for upload file

--- backend/src/main/java/com/mcmasterbaja/ExceptionMappers.java
+++ backend/src/main/java/com/mcmasterbaja/ExceptionMappers.java
@@ -69,4 +69,37 @@ public class ExceptionMappers {
 
     return Response.status(500).entity(errorResponse).type(MediaType.APPLICATION_JSON).build();
   }
+
+  // TODO: Put these in the upload resource file, since they are specific to that
+  @ServerExceptionMapper
+  public Response mapUnsatisfiedLink(UnsatisfiedLinkError e) {
+    String errorId = UUID.randomUUID().toString();
+    logger.error("errorId[{}]", errorId, e);
+
+    ErrorResponse errorResponse =
+        new ErrorResponse(
+            errorId,
+            e.getStackTrace()[6].getClassName() + "." + e.getStackTrace()[6].getMethodName(),
+            "Failed to link to parser library. Might need to restart the backend.",
+            "UNSATISFIED_LINK_ERROR",
+            e.getMessage());
+
+    return Response.status(500).entity(errorResponse).type(MediaType.APPLICATION_JSON).build();
+  }
+
+  @ServerExceptionMapper
+  public Response mapUnsatisfiedLink(NoClassDefFoundError e) {
+    String errorId = UUID.randomUUID().toString();
+    logger.error("errorId[{}]", errorId, e);
+
+    ErrorResponse errorResponse =
+        new ErrorResponse(
+            errorId,
+            e.getStackTrace()[0].getClassName() + "." + e.getStackTrace()[0].getMethodName(),
+            "Failed to link to parser library. Probably need to restart your backend, live reload for the parser does not work.",
+            "UNSATISFIED_LINK_ERROR",
+            e.getMessage());
+
+    return Response.status(500).entity(errorResponse).type(MediaType.APPLICATION_JSON).build();
+  }
 }
