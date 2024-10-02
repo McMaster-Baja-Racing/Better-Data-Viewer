ed76720d278c00d55331de013fff56111b811039
camdnnn <cameron634@gmail.com>
2024-10-02 12:48:03 -0400

    updating pathing for executable

--- front-end/src/electron/main.js
+++ front-end/src/electron/main.js
@@ -24,12 +24,12 @@ function createWindow() {
     
   if (isDev) {
     win.loadFile( path.resolve(__dirname, '../../build/index.html'));
+    backend = spawn('java', ['-jar', path.resolve(__dirname, '../../../backend/target/backend-1.2.0-runner.jar')]);
   } else {
     win.loadFile( path.resolve(__dirname, '/build/index.html'));
+    backend = spawn('java', ['-jar', path.resolve(__dirname, '/backend/target/backend-1.2.0-runner.jar')]);
   }
 
-  backend = spawn('java', ['-jar', path.resolve(__dirname, '../../../backend/target/backend-1.2.0-runner.jar')]);
-    
   win.on('closed', () => win = null);
 
 }
