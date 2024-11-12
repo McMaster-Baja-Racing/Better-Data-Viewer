a04a86c975c7c9575beb390a1f85ee3f4af037c8
Kai Arseneau <kaiarseneau@hotmail.com>
2024-11-11 20:33:59 -0500

    Debugging

--- e2e/tests/create/no-analyzer.test.ts
+++ e2e/tests/create/no-analyzer.test.ts
@@ -6,6 +6,10 @@ test.describe('Create graphs', () => {
   test.beforeEach(async ({ page }) => {
     const uploadTester = new UploadTester(page);
 
+    page.on('console', msg => {
+      console.log(`Console message: [${msg.type()}] ${msg.text()}`);
+    });
+
     await page.goto('http://localhost:5173/');
     await uploadTester.testUploadBinFile();
   });
