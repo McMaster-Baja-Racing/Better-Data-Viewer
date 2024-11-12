a9812d2b03bcfd57366c7c4b19f009448a276f89
Kai Arseneau <kaiarseneau@hotmail.com>
2024-11-11 22:30:56 -0500

    Added linux dll

--- backend/src/main/java/com/mcmasterbaja/binary_csv/BinaryToCSV.java
+++ backend/src/main/java/com/mcmasterbaja/binary_csv/BinaryToCSV.java
@@ -14,14 +14,18 @@ public class BinaryToCSV {
   static {
     String path = System.getProperty("user.dir");
 
-    if (System.getProperty("os.name").equals("Mac OS X")) {
-      path += relativePath + "/libbinary_to_csv_lib.dylib";
-    } else {
-      path += relativePath + "binary_to_csv_lib.dll";
+    // Determine the appropriate library extension based on the OS
+    String osName = System.getProperty("os.name").toLowerCase();
+    if (osName.contains("mac")) {
+        path += relativePath + "libbinary_to_csv_lib.dylib";
+    } else if (osName.contains("linux")) {
+        path += relativePath + "libbinary_to_csv_lib.so";
+    } else { // Default to Windows
+        path += relativePath + "binary_to_csv_lib.dll";
     }
 
     System.load(path);
-  }
+}
 
   public static void main(String[] args) {
     System.out.println(
