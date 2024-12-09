9c1e780fd9753a5921f69674700f9ba8b4489797
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-12-09 12:09:50 -0500

    refactored DeleteOutliers

--- backend/src/main/java/com/mcmasterbaja/analyzer/DeleteOutliers.java
+++ backend/src/main/java/com/mcmasterbaja/analyzer/DeleteOutliers.java
@@ -1,6 +1,7 @@
 package com.mcmasterbaja.analyzer;
 
 import com.mcmasterbaja.annotations.OnAnalyzerException;
+import com.mcmasterbaja.exceptions.HeaderException;
 import com.opencsv.CSVReader;
 import com.opencsv.ICSVWriter;
 import lombok.SneakyThrows;
@@ -30,6 +31,8 @@ public class DeleteOutliers extends Analyzer {
     ICSVWriter writer = getWriter(outputFiles[0]);
 
     String[] headers = reader.readNext();
+    if (headers==null) { throw new HeaderException("Failed to read headers from input file: " + inputFiles[0]); }
+
     int xAxisIndex = this.getColumnIndex(inputColumns[0], headers);
     writer.writeNext(headers);
 
