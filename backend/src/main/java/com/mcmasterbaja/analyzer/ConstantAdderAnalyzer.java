4b0cb41c76c131e2a164cf5c52d3969b124e21a9
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-12-09 12:10:31 -0500

    refactored ConstantAdder

--- backend/src/main/java/com/mcmasterbaja/analyzer/ConstantAdderAnalyzer.java
+++ backend/src/main/java/com/mcmasterbaja/analyzer/ConstantAdderAnalyzer.java
@@ -1,6 +1,7 @@
 package com.mcmasterbaja.analyzer;
 
 import com.mcmasterbaja.annotations.OnAnalyzerException;
+import com.mcmasterbaja.exceptions.HeaderException;
 import com.opencsv.CSVReader;
 import com.opencsv.ICSVWriter;
 import lombok.SneakyThrows;
@@ -39,6 +40,7 @@ public class ConstantAdderAnalyzer extends Analyzer {
     ICSVWriter writer = getWriter(super.outputFiles[0]);
     if (inputColumns.length == 4) {
       String[] headers = reader.readNext();
+      if (headers==null) { throw new HeaderException("Failed to read headers from input file: " + inputFiles[0]); }
       int aIndex = this.getColumnIndex(inputColumns[0], headers);
       int bIndex = this.getColumnIndex(inputColumns[1], headers);
       int cIndex = this.getColumnIndex(inputColumns[2], headers);
