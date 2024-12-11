897b22c16cbcc72aaa6b8b8ccd2ad516444a7e10
Cameron Dunn <cameron634@gmail.com>
2024-12-10 23:46:32 -0500

    fixed web baseApiUrl pathing

--- front-end/src/lib/electronUtils.ts
+++ front-end/src/lib/electronUtils.ts
@@ -1,10 +1,11 @@
 import { BrowserRouter, HashRouter } from "react-router-dom";
+console.log(typeof process !== 'undefined' && process.versions?.electron);
 
 // Determines if the app is running in an Electron environment
 const isElectron = typeof process !== 'undefined' && process.versions?.electron;
 
 // Determines what the base API URL should be based on the environment
-export const baseApiUrl = isElectron ? 'http://localhost:8080' : window.location.hostname + ':8080';
+export const baseApiUrl = 'http://' + (isElectron ? 'localhost' : window.location.hostname) + ':8080';
 
 // Determines what the base URL should be based on the environment
 export const RouterComponent = isElectron ? HashRouter : BrowserRouter;
