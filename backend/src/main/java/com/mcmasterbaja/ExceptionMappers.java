3b19e8a68b8b3a235045554add7779c9447604ca
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-12-22 16:43:43 -0500

    more specific user-facing messages

--- backend/src/main/java/com/mcmasterbaja/ExceptionMappers.java
+++ backend/src/main/java/com/mcmasterbaja/ExceptionMappers.java
@@ -1,16 +1,23 @@
 package com.mcmasterbaja;
 
+import java.util.UUID;
+
+import org.jboss.logging.Logger;
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+
 import com.mcmasterbaja.exceptions.AnalyzerException;
 import com.mcmasterbaja.exceptions.InvalidArgumentException;
+import com.mcmasterbaja.exceptions.InvalidColumnException;
+import com.mcmasterbaja.exceptions.InvalidHeaderException;
+import com.mcmasterbaja.exceptions.InvalidInputFileException;
+import com.mcmasterbaja.exceptions.InvalidOutputFileException;
 import com.mcmasterbaja.exceptions.MalformedCsvException;
 import com.mcmasterbaja.exceptions.StorageException;
 import com.mcmasterbaja.model.ErrorResponse;
+
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import java.util.UUID;
-import org.jboss.logging.Logger;
-import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
 // Exceptions are mapped in priority of most specific first
 public class ExceptionMappers {
@@ -29,6 +36,82 @@ public class ExceptionMappers {
         e.getStackTrace()[0].getClassName() + "." + e.getStackTrace()[0].getMethodName(),
         "An invalid argument was passed.",
         "INVALID_ARGUMENT",
+        e.getMessage());
+
+    return Response.status(Response.Status.BAD_REQUEST)
+        .entity(errorResponse)
+        .type(MediaType.APPLICATION_JSON)
+        .build();
+  }
+
+  // Handles invalid column exceptions
+  @ServerExceptionMapper
+  public Response invalidColumn(InvalidColumnException e) {
+    String errorId = UUID.randomUUID().toString();
+    logger.error("errorId[{}]", errorId, e);
+
+    ErrorResponse errorResponse = new ErrorResponse(
+        errorId,
+        e.getStackTrace()[0].getClassName() + "." + e.getStackTrace()[0].getMethodName(),
+        "An invalid column was specified.",
+        "INVALID_COLUMN",
+        e.getMessage());
+
+    return Response.status(Response.Status.BAD_REQUEST)
+        .entity(errorResponse)
+        .type(MediaType.APPLICATION_JSON)
+        .build();
+  }
+
+  // Handles invalid header exceptions
+  @ServerExceptionMapper
+  public Response headerException(InvalidHeaderException e) {
+    String errorId = UUID.randomUUID().toString();
+    logger.error("errorId[{}]", errorId, e);
+
+    ErrorResponse errorResponse = new ErrorResponse(
+        errorId,
+        e.getStackTrace()[0].getClassName() + "." + e.getStackTrace()[0].getMethodName(),
+        "Failed to read headers from input file.",
+        "HEADER_ERROR",
+        e.getMessage());
+
+    return Response.status(Response.Status.BAD_REQUEST)
+        .entity(errorResponse)
+        .type(MediaType.APPLICATION_JSON)
+        .build();
+  }
+
+  // Handles invalid input file exceptions
+  @ServerExceptionMapper
+  public Response invalidInputFile(InvalidInputFileException e) {
+    String errorId = UUID.randomUUID().toString();
+    logger.error("errorId[{}]", errorId, e);
+
+    ErrorResponse errorResponse = new ErrorResponse(
+        errorId,
+        e.getStackTrace()[0].getClassName() + "." + e.getStackTrace()[0].getMethodName(),
+        "An invalid input file was specified.",
+        "INVALID_INPUT_FILE",
+        e.getMessage());
+
+    return Response.status(Response.Status.BAD_REQUEST)
+        .entity(errorResponse)
+        .type(MediaType.APPLICATION_JSON)
+        .build();
+  }
+
+  // Handles invalid output file exceptions
+  @ServerExceptionMapper
+  public Response invalidOutputFile(InvalidOutputFileException e) {
+    String errorId = UUID.randomUUID().toString();
+    logger.error("errorId[{}]", errorId, e);
+
+    ErrorResponse errorResponse = new ErrorResponse(
+        errorId,
+        e.getStackTrace()[0].getClassName() + "." + e.getStackTrace()[0].getMethodName(),
+        "An invalid output file was specified.",
+        "INVALID_OUTPUT_FILE",
         e.getMessage());
 
     return Response.status(Response.Status.BAD_REQUEST)
