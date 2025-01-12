c44ede996b768a0f5efff196c8f26ac0a8e6b8d8
Kai Arseneau <kaiarseneau@hotmail.com>
2025-01-11 21:59:31 -0500

    Added fix link

--- front-end/src/types.d.ts
+++ front-end/src/types.d.ts
@@ -1,8 +1,11 @@
+// Can also look into more strict approaches such as typed-scss-modules
 declare module '*.scss' {
   const classes: Record<string, string>;
   export default classes;
 }
 
+
+// This fix is given by https://github.com/pmndrs/react-three-fiber/issues/3385#issuecomment-2446045646
 import { ThreeElements } from '@react-three/fiber'
 
 declare global {
@@ -13,5 +16,3 @@ declare global {
     }
   }
 }
-
-// Can also look into more strict approaches such as typed-scss-modules
