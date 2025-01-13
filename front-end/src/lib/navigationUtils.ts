fe9e75ec9a46bcbc2ab7bd0548a4387b6be47963
Kai Arseneau <kaiarseneau@hotmail.com>
2025-01-13 16:35:02 -0500

    Removing console.log

--- front-end/src/lib/navigationUtils.ts
+++ front-end/src/lib/navigationUtils.ts
@@ -1,5 +1,4 @@
 import { BrowserRouter, HashRouter } from "react-router-dom";
-console.log(typeof process !== 'undefined' && process.versions?.electron);
 
 // Determines if the app is running in an Electron environment
 export const isElectron = typeof process !== 'undefined' && process.versions?.electron;
