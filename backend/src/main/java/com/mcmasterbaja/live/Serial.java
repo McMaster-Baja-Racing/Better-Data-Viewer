e7c5f3aff4784db17632c282cc1f06fe26ffc9fb
Vedant Jhawar <vedantj1203@gmail.com>
2025-01-21 10:08:18 -0500

    Cleaned up some stuff

--- backend/src/main/java/com/mcmasterbaja/live/Serial.java
+++ backend/src/main/java/com/mcmasterbaja/live/Serial.java
@@ -29,16 +29,28 @@ public class Serial {
           break;
         }
       }
-      if (!setPort) {
-        setPort = true;
+      if (!setPort) { // throwing exception if port not found
+        throw new Exception("No suitable port found")
       }
     }
 
-    comPort.setBaudRate(115200);
-    boolean connected = comPort.openPort();
-    System.out.println(connected);
-    // comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 10000, 0);
-    comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 2000, 0);
+  try {
+      comPort.setBaudRate(115200);
+      if (comPort.openPort()) {
+        System.out.println("Connected to port"); 
+        comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 2000, 0);
+      } else {
+        System.out.println("Could not connect to port.")
+      }
+  } catch (Exception E) {
+    System.out.println("Error during port setup");
+    E.printStackTrace(); 
+  } finally {
+    if (comPort.openPort()) {
+      comPort.closePort(); 
+      System.out.println("Port closed");
+    }
+  }
 
     FileWriter fw = null;
     FileWriter fw2 = null;
@@ -84,52 +96,43 @@ public class Serial {
     try {
       while (!exit) {
         byte[] readBuffer = new byte[8];
+        int numRead = comPort.readBytes(readBuffer, readBuffer.length);
 
-        // int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+        System.out.println("Read " + numRead + " bytes. Number of Bytes: " + readBuffer.length+ "
+        Bytes: " + readBuffer[0] + ", " + readBuffer[1] + ", " + readBuffer[2] + ", " +
+        readBuffer[3] + ", " + readBuffer[4] + ", " + readBuffer[5] + ", " + readBuffer[6] + ", "
+        + readBuffer[7] );
 
-        // System.out.println("Read " + numRead + " bytes. Number of Bytes: " +
-        // readBuffer.length+ "
-        // Bytes: " + readBuffer[0] + ", " + readBuffer[1] + ", " + readBuffer[2] + ", "
-        // +
-        // readBuffer[3] + ", " + readBuffer[4] + ", " + readBuffer[5] + ", " +
-        // readBuffer[6] + ", "
-        // + readBuffer[7] );
-        // make a packet object from the byte array
-
-        // make a new byte array that is a flipped version of readBuffer
         Packet p = new Packet(readBuffer);
-        // System.out.println(p.getTimestamp() + ", " + p.getPacketType() + ", " +
-        // p.getFloatData());
-        if (p.getPacketType() == 37) {
-          // write the timestamp and the float data to the file
-          fw.write(p.getTimestamp() + "," + p.getFloatData() + "\n");
-          // flush the file writer
-          fw.flush();
-        } else if (p.getPacketType() == 36) {
-          // write the timestamp and the float data to the file
-          fw2.write(p.getTimestamp() + "," + p.getFloatData() + "\n");
-          // flush the file writer
-          fw2.flush();
+        System.out.println(p.getTimestamp() + ", " + p.getPacketType() + ", " + p.getFloatData());
 
-        } else if (p.getPacketType() == 42) {
-          // write the timestamp and the float data to the file
-          fw42.write(p.getTimestamp() + "," + p.getFloatData() + "\n");
-          // flush the file writer
-          fw42.flush();
-        } else if (p.getPacketType() >= 28 && p.getPacketType() <= 33) {
-          // System.out.println("Read " + numRead + " bytes. Number of Bytes: " +
-          // readBuffer.length+
-          // " Bytes: " + readBuffer[0] + ", " + readBuffer[1] + ", " + readBuffer[2] + ",
-          // " +
-          // readBuffer[3] + ", " + readBuffer[4] + ", " + readBuffer[5] + ", " +
-          // readBuffer[6] + ",
-          // " + readBuffer[7] );
-          // System.out.println(p.getTimestamp() + ", " + p.getPacketType() + ", " +
-          // p.getFloatData());
-          // write the timestamp and the float data to the file
-          strains[p.getPacketType() - 28].write(p.getTimestamp() + "," + p.getFloatData() + "\n");
-          // flush the file writer
-          strains[p.getPacketType() - 28].flush();
+        switch (p.getPacketType()) {
+          case 37: 
+            fw.write(p.getTimestamp() + "," + p.getFloatData() + "\n");
+            fw.flush();
+            break;
+
+          case 36: 
+            fw2.write(p.getTimestamp() + "," + p.getFloatData() + "\n");
+            fw2.flush();  
+            break; 
+
+          case 42: 
+            fw42.write(p.getTimestamp() + "," + p.getFloatData() + "\n");
+            fw42.flush();
+            break; 
+          
+          default: 
+            if (p.getPacketType() >= 28 && p.getPacketType() <= 33) {
+              System.out.println("Read " + numRead + " bytes. Number of Bytes: " + readBuffer.length+
+              " Bytes: " + readBuffer[0] + ", " + readBuffer[1] + ", " + readBuffer[2] + ", " +
+              readBuffer[3] + ", " + readBuffer[4] + ", " + readBuffer[5] + ", " + readBuffer[6] + ",
+              " + readBuffer[7] );
+              System.out.println(p.getTimestamp() + ", " + p.getPacketType() + ", " +
+              p.getFloatData());
+              strains[p.getPacketType() - 28].write(p.getTimestamp() + "," + p.getFloatData() + "\n");
+              strains[p.getPacketType() - 28].flush();
+            }
         }
       }
     } catch (Exception e) {
@@ -148,80 +151,65 @@ public class Serial {
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+  public static void readLive() { // processes data from COM5 and safely shuts down
+    String port = "COM5";
+    SerialPort comPort = SerialPort.getCommPort(port);
+    comPort.openPort();
+    comPort.closePort();
+    comPort.openPort();
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {comPort.closePort();}));
 
-    // exit = false;
+    try {
+        Thread.sleep(1000);
+        while (true)
+        {
+        //print the number of bytes available
+        System.out.println(comPort.bytesAvailable() + " bytes available");
+        Thread.sleep(2000);
+            while (comPort.bytesAvailable() == 0)
+              Thread.sleep(20);
+
+            if (comPort.bytesAvailable() >=8) {
+                byte[] readBuffer = new byte[8];
+                int numRead = comPort.readBytes(readBuffer, 8);
+                System.out.println("Read " + numRead + " bytes.");
+                System.out.println(bytesToHex(readBuffer));
+                Packet p = new Packet(readBuffer);
+                System.out.println(p.getTimestamp() + ", " + p.getPacketType() + ", " + p.getFloatData());
+
+            }
+        }
+    } catch (Exception e) { e.printStackTrace(); };
   }
 
-  /*
-   *
-   * public static void readLive() {
-   *
-   *
-   *
-   * String port = "COM5";
-   * SerialPort comPort = SerialPort.getCommPort(port);
-   * comPort.openPort();
-   * comPort.closePort();
-   * comPort.openPort();
-   * Runtime.getRuntime().addShutdownHook(new Thread(() ->
-   * {comPort.closePort();}));
-   *
-   * try {
-   * Thread.sleep(1000);
-   * while (true)
-   * {
-   * //print the number of bytes available
-   * System.out.println(comPort.bytesAvailable() + " bytes available");
-   * Thread.sleep(2000);
-   * while (comPort.bytesAvailable() == 0)
-   * Thread.sleep(20);
-   *
-   * /*if (comPort.bytesAvailable() >=8) {
-   * byte[] readBuffer = new byte[8];
-   * int numRead = comPort.readBytes(readBuffer, 8);
-   * System.out.println("Read " + numRead + " bytes.");
-   * System.out.println(bytesToHex(readBuffer));
-   * Packet p = new Packet(readBuffer);
-   * System.out.println(p.getTimestamp() + ", " + p.getPacketType() + ", " +
-   * p.getFloatData());
-   *
-   * }
-   * }
-   * } catch (Exception e) { e.printStackTrace(); };
-   * }
-   */
+  public static void readLive() {
+    SerialPort comPort = SerialPort.getCommPort("COM5");
+    comPort.setBaudRate(115200);
+    comPort.openPort();
+    comPort.addDataListener(new SerialPortPacketListener() {
+        @Override
+        public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_RECEIVED; }
 
-  /*
-   * public static void readLive() {
-   * SerialPort comPort = SerialPort.getCommPort("COM5");
-   * comPort.setBaudRate(115200);
-   * comPort.openPort();
-   * comPort.addDataListener(new SerialPortPacketListener() {
-   *
-   * @Override
-   * public int getListeningEvents() { return
-   * SerialPort.LISTENING_EVENT_DATA_RECEIVED; }
-   *
-   * @Override
-   * public int getPacketSize() {
-   * return 8;
-   * }
-   *
-   * @Override
-   * public void serialEvent(SerialPortEvent event)
-   * {
-   * byte[] newData = event.getReceivedData();
-   * System.out.println("Received data of size: " + newData.length);
-   * for (int i = 0; i < newData.length; ++i)
-   * System.out.print((char)newData[i]);
-   * System.out.println("\n");
-   * Packet p = new Packet(newData);
-   * System.out.println(p.getTimestamp() + ", " + p.getPacketType() + ", " +
-   * p.getFloatData());
-   * }
-   * });
-   * }
-   */
+        @Override
+        public int getPacketSize() {
+            return 8;
+        }
+
+        @Override
+        public void serialEvent(SerialPortEvent event)
+        {
+            byte[] newData = event.getReceivedData();
+            System.out.println("Received data of size: " + newData.length);
+            for (int i = 0; i < newData.length; ++i)
+                System.out.print((char)newData[i]);
+            System.out.println("\n");
+            Packet p = new Packet(newData);
+            System.out.println(p.getTimestamp() + ", " + p.getPacketType() + ", " + p.getFloatData());
+        }
+    });
+}
+
   public static byte[] readSerial() {
     return null;
   }
