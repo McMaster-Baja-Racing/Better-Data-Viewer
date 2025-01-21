deb58af3b5d61943c923bc6f0a5f6190db0fd5ad
t-wing11 <Traviswing20@gmail.com>
2025-01-21 00:04:58 -0500

    option square code
    Added new hover color to the variables
    Added temp illustration as a placeholder

---
+++ front-end/src/components/optionSquare/optionSquare.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import styles from './optionSquare.module.scss';
+
+interface OptionSquareProps {
+  label: string;
+  illustration?: string;
+  isClicked?: boolean;
+}
+
+export const OptionSquare: React.FC<OptionSquareProps> = ({ label, illustration, isClicked}) => {
+    const [clicked, setClicked] = useState(isClicked);  // To track the click state
+
+    const handleClick = () => {
+        setClicked(prev => !prev); // Toggle clicked state
+    };
+    
+    return (
+<div className={`${styles.optionSquare} ${clicked ? styles.active : ''}`}  onClick={handleClick}>            
+            <div className={styles.illustration}>
+                {illustration ? <img src={illustration} alt="Illustration" /> : null}
+            </div>
+            <div className={styles.label}>
+                   {label}
+            </div>
+        </div>
+    );
+}
