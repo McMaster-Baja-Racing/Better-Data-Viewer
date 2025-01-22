62f3d65f7b49d29edc1bda95f85f4021211fb423
camdnnn <cameron634@gmail.com>
2025-01-21 19:38:03 -0500

    added hovering

--- front-end/src/components/sidebar/Sidebar.tsx
+++ front-end/src/components/sidebar/Sidebar.tsx
@@ -1,42 +1,57 @@
-import './Sidebar.module.scss';
-import cx from 'classnames';
-import { useState } from 'react';
+import styles from './Sidebar.module.scss';
+import { onIconClick } from '@lib/navigationUtils';
 import bajaLogo from '@assets/baja_logo.svg'
 import homeIcon from '@assets/icons/home.svg';
-
-// defines the properties of an item in the sidebar
-type SidebarItem = {
-    icon: string;
-    name: string;
-    onClick: () => void;
-}
+import bookmarkIcon from '@assets/icons/bookmark.svg';
+import downloadIcon from '@assets/icons/download.svg';
+import uploadIcon from '@assets/icons/upload.svg';
+import folderIcon from '@assets/icons/folder.svg';
+import settingsIcon from '@assets/icons/settings.svg';
+import accountIcon from '@assets/icons/account.svg';
 
 const Sidebar = () => {
-    // controls the open or closed state of the sidebar
-    const [isOpen, setIsOpen] = useState(false);
-    
-    // list the items that will be displayed in the sidebar
-    const items: SidebarItem[] = [
-        {
-            icon: homeIcon,
-            name: 'Home',
-            onClick: () => console.log('Home clicked')
-        }
-    ]
+    // Defines the image, text and linked path for a sidebar item
+    interface SidebarItemProps {
+        icon: string;
+        text: string;
+        link?: string;
+    }
 
-    const buttonizeItem = (item: SidebarItem, index: number) => {
+    // Defines the sidebar item component function and styling
+    const SidebarItem = ({ icon, text, link }: SidebarItemProps) => {
         return (
-            <button key={index} onClick={item.onClick}>
-                <img src={item.icon} alt={item.name} />
-                <span>{item.name}</span>
+            <button className={styles.item} onClick={() => { if(link) onIconClick(link) }}>
+                <img className={styles.icon} src={icon} />
+                <span className={styles.text}>{text}</span>
             </button>
         )
     }
 
     return (
-        <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-            <img src={bajaLogo} alt='Baja Logo' />
-            {items.map((item, index) => buttonizeItem(item, index))}
+        <div className={styles.sidebar}>
+            <div className={styles.header}>
+                <img className={styles.logo} src={bajaLogo} />
+                <span className={styles.title}>Data Viewer</span>
+            </div>
+            <div className={styles.body}>
+                <div className={styles.section}>
+                    <span className={styles.sectionTitle}>Overview</span>
+                </div>
+                <SidebarItem icon={homeIcon} text="Home" link="" />
+                <SidebarItem icon={bookmarkIcon} text="Bookmarked"/>
+                <SidebarItem icon={downloadIcon} text="Download Data"/>
+                <SidebarItem icon={folderIcon} text="File Browser"/>
+                <SidebarItem icon={uploadIcon} text="Upload Data"/>
+            </div>
+            <div className={styles.footer}>
+            <div className={styles.section}>
+                    <span className={styles.sectionTitle}>Account</span>
+                </div>
+                <SidebarItem icon={settingsIcon} text="Settings"/>
+                <SidebarItem icon={accountIcon} text="My Account"/>
+            </div>
         </div>
     )
 }
+
+export default Sidebar;
