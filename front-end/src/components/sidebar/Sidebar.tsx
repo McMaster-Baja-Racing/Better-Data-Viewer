c3cb7375aa48519abb4d4be7ed44c9cadba5f9a5
Cameron Dunn <cameron634@gmail.com>
2025-01-23 18:42:48 -0500

    adding more clicking

--- front-end/src/components/sidebar/Sidebar.tsx
+++ front-end/src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.scss';
-import { onIconClick } from '@lib/navigationUtils';
+import cx from 'classnames';
+import { useState } from 'react';
 import bajaLogo from '@assets/baja_logo.svg'
 import homeIcon from '@assets/icons/home.svg';
 import bookmarkIcon from '@assets/icons/bookmark.svg';
@@ -14,41 +15,46 @@ const Sidebar = () => {
     interface SidebarItemProps {
         icon: string;
         text: string;
-        link?: string;
+        onClick?: () => void;
     }
 
     // Defines the sidebar item component function and styling
-    const SidebarItem = ({ icon, text, link }: SidebarItemProps) => {
+    const SidebarItem = ({ icon, text, onClick }: SidebarItemProps) => {
         return (
-            <button className={styles.item} onClick={() => { if(link) onIconClick(link) }}>
-                <img className={styles.icon} src={icon} />
+            <button className={styles.item} onClick={onClick}>
+                <div className={styles.iconContainer}>
+                    <img className={styles.icon} src={icon} />
+                </div>
                 <span className={styles.text}>{text}</span>
             </button>
         )
     }
 
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleSidebar = () => setIsOpen(!isOpen);
+
     return (
-        <div className={styles.sidebar}>
-            <div className={styles.header}>
+        <div className={cx(styles.sidebar, { [styles.open]: isOpen })}>
+            <div className={styles.header} onClick={toggleSidebar}>
                 <img className={styles.logo} src={bajaLogo} />
-                <span className={styles.title}>Data Viewer</span>
+                <span className={styles.title}>DATA VIEWER</span>
             </div>
             <div className={styles.body}>
                 <div className={styles.section}>
-                    <span className={styles.sectionTitle}>Overview</span>
+                    <span className={styles.sectionTitle}>OVERVIEW</span>
                 </div>
-                <SidebarItem icon={homeIcon} text="Home" link="" />
-                <SidebarItem icon={bookmarkIcon} text="Bookmarked"/>
-                <SidebarItem icon={downloadIcon} text="Download Data"/>
-                <SidebarItem icon={folderIcon} text="File Browser"/>
-                <SidebarItem icon={uploadIcon} text="Upload Data"/>
+                <SidebarItem icon={homeIcon} text="Home" onClick={() => console.log('home')} />
+                <SidebarItem icon={bookmarkIcon} text="Bookmarked" onClick={() => console.log('bookmarked')}/>
+                <SidebarItem icon={downloadIcon} text="Download Data" onClick={() => console.log('download data')}/>
+                <SidebarItem icon={folderIcon} text="File Browser" onClick={() => console.log('file browser')}/>
+                <SidebarItem icon={uploadIcon} text="Upload Data" onClick={() => console.log('upload data')}/>
             </div>
             <div className={styles.footer}>
             <div className={styles.section}>
-                    <span className={styles.sectionTitle}>Account</span>
+                    <span className={styles.sectionTitle}>ACCOUNT</span>
                 </div>
-                <SidebarItem icon={settingsIcon} text="Settings"/>
-                <SidebarItem icon={accountIcon} text="My Account"/>
+                <SidebarItem icon={settingsIcon} text="Settings" onClick={() => console.log('settings')}/>
+                <SidebarItem icon={accountIcon} text="My Account" onClick={() => console.log('my account')}/>
             </div>
         </div>
     )
