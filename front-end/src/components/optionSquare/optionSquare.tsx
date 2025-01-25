b03acc781bb49135325220987f1351332cfd2bf4
t-wing11 <Traviswing20@gmail.com>
2025-01-25 17:38:54 -0500

    PR Comments

--- front-end/src/components/optionSquare/optionSquare.tsx
+++ front-end/src/components/optionSquare/optionSquare.tsx
@@ -1,23 +1,23 @@
 import { useState } from 'react';
 import styles from './optionSquare.module.scss';
-import graphImage from '../../assets/graph_image.png';
+import cx from 'classnames';
 
 interface OptionSquareProps {
-  label?: string;
-  illustration?: string;
-  isClicked?: boolean;
+  label: string;
+  illustration: string;
+  isClicked: boolean;
 }
 
-export const OptionSquare: React.FC<OptionSquareProps> = ({ label = "Option Title", illustration = graphImage, isClicked}) => {
-    const [clicked, setClicked] = useState(isClicked);  // To track the click state
+export const OptionSquare = ({ label, illustration, isClicked}: OptionSquareProps) => {
+    const [clicked, setClicked] = useState(isClicked); 
 
     const handleClick = () => {
-        setClicked(prev => !prev); // Toggle clicked state
+        setClicked(prev => !prev);
     };
     
     return (
-<div className={`${styles.optionSquare} ${clicked ? styles.active : ''}`}  onClick={handleClick}>            
-            <div className={styles.illustration}>
+        <div className={cx(styles.optionSquare, { [styles.clicked]: clicked })} onClick={handleClick}>
+        <div className={styles.illustration}>
                 {illustration ? <img src={illustration} alt="Illustration" /> : null}
             </div>
             <div className={styles.label}>
