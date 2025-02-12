8af5ce111c11e26227afffdb9e4f5f666abe5b7d
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-02-12 18:19:22 -0500

    more scss

--- front-end/src/components/graphHeader/graphHeader.tsx
+++ front-end/src/components/graphHeader/graphHeader.tsx
@@ -10,8 +10,11 @@ interface GraphHeaderProps {
       <div className={styles.graphHeader}>
         <div className={styles['title-group']}>
             Drivetrain Preset
+
         </div>
-        
+        <div className={styles.buttons}>
+                meow
+            </div>
       </div>
     );
   }
