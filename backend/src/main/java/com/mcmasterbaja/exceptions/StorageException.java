4561bc233d9aa358b9b8e549f44d8376ac055891
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-02-18 18:05:35 -0500

    storage exception message fix

--- backend/src/main/java/com/mcmasterbaja/exceptions/StorageException.java
+++ backend/src/main/java/com/mcmasterbaja/exceptions/StorageException.java
@@ -3,7 +3,7 @@ package com.mcmasterbaja.exceptions;
 public class StorageException extends BajaException {
 
   private static final String DEFAULT_ERROR_CODE = "STORAGE_ERROR";
-  private static final String DEFAULT_USER_MESSAGE = "An error occurred with storage.";
+  private static final String DEFAULT_USER_MESSAGE = "Something went wrong. It's probably your fault.";
 
   public StorageException(String message) {
     super(message, DEFAULT_ERROR_CODE, DEFAULT_USER_MESSAGE);
