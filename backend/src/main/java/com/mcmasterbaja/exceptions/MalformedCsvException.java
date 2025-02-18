748c0edb9673dce5e8e3fd5dd9f33dee64efbbce
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-02-18 13:52:32 -0500

    fixed malformedcsv

--- backend/src/main/java/com/mcmasterbaja/exceptions/MalformedCsvException.java
+++ backend/src/main/java/com/mcmasterbaja/exceptions/MalformedCsvException.java
@@ -4,21 +4,18 @@ import lombok.Getter;
 
 @Getter
 public class MalformedCsvException extends StorageException {
-  private static final String ERROR_CODE = "MALFORMED_CSV";
-  private static final String USER_MESSAGE = "The CSV file is malformed.";
-
   private String file;
 
   public MalformedCsvException(String message) {
-    super(message, ERROR_CODE, USER_MESSAGE);
+    super(message);
   }
 
   public MalformedCsvException(String message, Throwable cause) {
-    super(message, cause, ERROR_CODE, USER_MESSAGE);
+    super(message, cause);
   }
 
   public MalformedCsvException(String message, String file, Throwable cause) {
-    super(message, cause, ERROR_CODE, USER_MESSAGE);
+    super(message, cause);
     this.file = file;
   }
 }
