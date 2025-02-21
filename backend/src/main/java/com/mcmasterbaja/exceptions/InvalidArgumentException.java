feab9dc696049852b6f8b73cab3237b97b015f9c
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-02-21 16:39:15 -0500

    refactor InvalidArgumentException to use local vars for message + code

--- backend/src/main/java/com/mcmasterbaja/exceptions/InvalidArgumentException.java
+++ backend/src/main/java/com/mcmasterbaja/exceptions/InvalidArgumentException.java
@@ -3,27 +3,25 @@ package com.mcmasterbaja.exceptions;
 import java.util.List;
 
 public class InvalidArgumentException extends BajaException {
+
+  private static final String ERROR_CODE = "INVALID_ARGUMENT";
+  private static final String USER_MESSAGE_LIST = "One or more invalid arguments were passed.";
+  private static final String USER_MESSAGE_SINGLE = "An invalid argument was passed.";
+
   private final List<String> errors;
 
   public InvalidArgumentException(List<String> errors) {
-    super(
-        String.join(", ", errors),
-        "INVALID_ARGUMENT",
-        "One or more invalid arguments were passed.");
+    super(String.join(", ", errors), ERROR_CODE, USER_MESSAGE_LIST);
     this.errors = errors;
   }
 
   public InvalidArgumentException(String error) {
-    super(error, "INVALID_ARGUMENT", "An invalid argument was passed.");
+    super(error, ERROR_CODE, USER_MESSAGE_SINGLE);
     this.errors = List.of(error);
   }
 
   public InvalidArgumentException(List<String> errors, Throwable cause) {
-    super(
-        String.join(", ", errors),
-        cause,
-        "INVALID_ARGUMENT",
-        "One or more invalid arguments were passed.");
+    super(String.join(", ", errors), cause, ERROR_CODE, USER_MESSAGE_LIST);
     this.errors = errors;
   }
 
