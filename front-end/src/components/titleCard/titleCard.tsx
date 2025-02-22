7cb6ea93cee266e9378322e0f783dda70303d25a
t-wing11 <Traviswing20@gmail.com>
2025-02-22 17:49:57 -0500

    added proper gradients

--- front-end/src/components/titleCard/titleCard.tsx
+++ front-end/src/components/titleCard/titleCard.tsx
@@ -1,21 +1,30 @@
 import { useState } from 'react';
 import styles from './titleCard.module.scss';
 import cx from 'classnames';
+import Chart from '../views/Chart/Chart';
 
 interface TitleCardProps {
-    title: string;
     description: string;   
 }
 
-export const TitleCard = ({ title, description }: TitleCardProps) => {
+export const TitleCard = ({ description }: TitleCardProps) => {
     return (
         <div className={styles.titleCard}>
            <div className={styles.textContainer}>
-            <div className={styles.title}>{title}</div>
+            <div className={styles.title}>
+            <span className={styles.highlight1}>VISUALIZE</span> YOUR <br/>
+            DATA, <span className={styles.highlight2}>YOUR WAY </span>
+            </div>
             <div className={styles.description}>{description}</div>
             </div>
             <div className={styles.graph}>
-                <p> yooo</p>
+                <Chart 
+                    data={[]}
+                    options={{}}
+                    type="line"
+                    height={200}
+                    width={200}
+                />
             </div>
         </div>
     );
