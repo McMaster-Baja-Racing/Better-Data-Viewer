cfc656c1cd04fa0c853ea4f0ef5dc92ac39cc78d
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-02-24 17:23:04 -0500

    getReader + getWriter use try-with-resources and functional interfaces

--- backend/src/main/java/com/mcmasterbaja/analyzer/Analyzer.java
+++ backend/src/main/java/com/mcmasterbaja/analyzer/Analyzer.java
@@ -1,5 +1,12 @@
 package com.mcmasterbaja.analyzer;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.function.Consumer;
+
 import com.mcmasterbaja.exceptions.InvalidColumnException;
 import com.mcmasterbaja.exceptions.InvalidInputFileException;
 import com.mcmasterbaja.exceptions.InvalidOutputFileException;
@@ -9,11 +16,6 @@ import com.opencsv.CSVReaderBuilder;
 import com.opencsv.CSVWriter;
 import com.opencsv.CSVWriterBuilder;
 import com.opencsv.ICSVWriter;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public abstract class Analyzer {
   protected String[] inputFiles;
@@ -29,18 +31,20 @@ public abstract class Analyzer {
     this.outputFiles = params.getOutputFiles();
   }
 
-  public CSVReader getReader(String filePath) {
-    try {
-      FileReader fileReader = new FileReader(filePath);
-      BufferedReader bufferedReader = new BufferedReader(fileReader);
-      return new CSVReaderBuilder(bufferedReader).withSkipLines(0).build();
+  public void getReader(String filePath, Consumer<CSVReader> action) {
+    try (
+        FileReader fileReader = new FileReader(filePath);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        CSVReader reader = new CSVReaderBuilder(bufferedReader).withSkipLines(0).build()) {
+      action.accept(reader);
     } catch (IOException e) {
       throw new InvalidInputFileException("Failed to read input file: " + filePath, e);
     }
   }
 
   /**
-   * Default behaviour is to use the 0th output file, will need to be overridden in some special
+   * Default behaviour is to use the 0th output file, will need to be overridden
+   * in some special
    * cases
    *
    * @return Filename of the analyzer output
@@ -49,16 +53,17 @@ public abstract class Analyzer {
     return this.outputFiles[0];
   }
 
-  public ICSVWriter getWriter(String filePath) {
-    try {
-      FileWriter fileWriter = new FileWriter(filePath);
-      BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-      return new CSVWriterBuilder(bufferedWriter)
-          .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
-          .withQuoteChar(CSVWriter.NO_QUOTE_CHARACTER)
-          .withEscapeChar(CSVWriter.DEFAULT_ESCAPE_CHARACTER)
-          .withLineEnd(CSVWriter.DEFAULT_LINE_END)
-          .build();
+  public void getWriter(String filePath, Consumer<ICSVWriter> action) {
+    try (
+        FileWriter fileWriter = new FileWriter(filePath);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        ICSVWriter writer = new CSVWriterBuilder(bufferedWriter)
+            .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
+            .withQuoteChar(CSVWriter.NO_QUOTE_CHARACTER)
+            .withEscapeChar(CSVWriter.DEFAULT_ESCAPE_CHARACTER)
+            .withLineEnd(CSVWriter.DEFAULT_LINE_END)
+            .build()) {
+      action.accept(writer);
     } catch (IOException e) {
       throw new InvalidOutputFileException("Failed to write to output file: " + filePath, e);
     }
