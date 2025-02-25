9d7d939db73e1acd6e66831606c38bbe9453fbda
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-02-25 15:10:24 -0500

    refactor BullshitAnalyzer

--- backend/src/main/java/com/mcmasterbaja/analyzer/BullshitAnalyzer.java
+++ backend/src/main/java/com/mcmasterbaja/analyzer/BullshitAnalyzer.java
@@ -1,12 +1,16 @@
 package com.mcmasterbaja.analyzer;
 
+import org.jboss.logging.Logger;
+
 import com.mcmasterbaja.annotations.OnAnalyzerException;
 import com.mcmasterbaja.exceptions.InvalidHeaderException;
 import com.mcmasterbaja.model.AnalyzerParams;
+import com.opencsv.CSVReader;
+import com.opencsv.ICSVWriter;
+
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import lombok.SneakyThrows;
-import org.jboss.logging.Logger;
 
 @Dependent
 @OnAnalyzerException
@@ -19,7 +23,8 @@ public class BullshitAnalyzer extends Analyzer {
 
   private double numPoints;
 
-  @Inject Logger logger;
+  @Inject
+  Logger logger;
 
   @SneakyThrows
   public void analyze(AnalyzerParams params) {
@@ -40,45 +45,50 @@ public class BullshitAnalyzer extends Analyzer {
           getWriter(
               super.outputFiles[0],
               writer -> {
-                String[] headers = safeReadNext(reader);
-                if (headers == null) {
-                  throw new InvalidHeaderException(
-                      "Failed to read headers from input file: " + inputFiles[0]);
-                }
-
-                int xAxisIndex = this.getColumnIndex(inputColumns[0], headers);
-                int yAxisIndex = this.getColumnIndex(inputColumns[1], headers);
-
-                writer.writeNext(headers);
-
-                String[] lastDataPoint = safeReadNext(reader);
-                String[] dataPoint;
-
-                while ((dataPoint = safeReadNext(reader)) != null) {
-                  double prevX = Double.parseDouble(lastDataPoint[xAxisIndex]);
-                  double currX = Double.parseDouble(dataPoint[xAxisIndex]);
-                  double prevY = Double.parseDouble(lastDataPoint[yAxisIndex]);
-                  double currY = Double.parseDouble(dataPoint[yAxisIndex]);
-
-                  double realNumPoints = Math.abs(currX - prevX) * numPoints;
-
-                  for (int i = 0; i < realNumPoints; i++) {
-                    double xValue = prevX + ((currX - prevX) * (1.0 / realNumPoints) * i);
-
-                    double slope = (currY - prevY) / (currX - prevX);
-                    double yValue = prevY + (xValue - prevX) * slope;
-
-                    // TODO: This noise is relative to the yValue, so it will be more pronounced for
-                    // larger
-                    // values. Downside is higher y-values have higher noise
-                    double noise = yValue * (0.75 + Math.random() * 0.5);
-                    writer.writeNext(
-                        new String[] {Double.toString(xValue), Double.toString(noise)});
-                  }
-
-                  lastDataPoint = dataPoint;
-                }
+                bullshitIO(reader, writer);
               });
         });
+  }
+
+  @SneakyThrows
+  public void bullshitIO(CSVReader reader, ICSVWriter writer) {
+    String[] headers = reader.readNext();
+    if (headers == null) {
+      throw new InvalidHeaderException(
+          "Failed to read headers from input file: " + inputFiles[0]);
+    }
+
+    int xAxisIndex = this.getColumnIndex(inputColumns[0], headers);
+    int yAxisIndex = this.getColumnIndex(inputColumns[1], headers);
+
+    writer.writeNext(headers);
+
+    String[] lastDataPoint = reader.readNext();
+    String[] dataPoint;
+
+    while ((dataPoint = reader.readNext()) != null) {
+      double prevX = Double.parseDouble(lastDataPoint[xAxisIndex]);
+      double currX = Double.parseDouble(dataPoint[xAxisIndex]);
+      double prevY = Double.parseDouble(lastDataPoint[yAxisIndex]);
+      double currY = Double.parseDouble(dataPoint[yAxisIndex]);
+
+      double realNumPoints = Math.abs(currX - prevX) * numPoints;
+
+      for (int i = 0; i < realNumPoints; i++) {
+        double xValue = prevX + ((currX - prevX) * (1.0 / realNumPoints) * i);
+
+        double slope = (currY - prevY) / (currX - prevX);
+        double yValue = prevY + (xValue - prevX) * slope;
+
+        // TODO: This noise is relative to the yValue, so it will be more pronounced for
+        // larger
+        // values. Downside is higher y-values have higher noise
+        double noise = yValue * (0.75 + Math.random() * 0.5);
+        writer.writeNext(
+            new String[] { Double.toString(xValue), Double.toString(noise) });
+      }
+
+      lastDataPoint = dataPoint;
+    }
   }
 }
