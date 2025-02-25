3440e10b0c4adb71b40f3a2679a2d9bdd54ffde0
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-02-25 11:08:40 -0500

    refactor LinearMultiplyAnalyzer

--- backend/src/main/java/com/mcmasterbaja/analyzer/LinearMultiplyAnalyzer.java
+++ backend/src/main/java/com/mcmasterbaja/analyzer/LinearMultiplyAnalyzer.java
@@ -1,15 +1,15 @@
 package com.mcmasterbaja.analyzer;
 
+import org.jboss.logging.Logger;
+
 import com.mcmasterbaja.annotations.OnAnalyzerException;
 import com.mcmasterbaja.exceptions.InvalidHeaderException;
 import com.mcmasterbaja.model.AnalyzerParams;
 import com.mcmasterbaja.model.AnalyzerType;
-import com.opencsv.CSVReader;
-import com.opencsv.ICSVWriter;
+
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import lombok.SneakyThrows;
-import org.jboss.logging.Logger;
 
 @Dependent
 @AnalyzerQualifier(AnalyzerType.LINEAR_MULTIPLY)
@@ -18,7 +18,8 @@ public class LinearMultiplyAnalyzer extends Analyzer {
   private double m;
   private double b;
 
-  @Inject Logger logger;
+  @Inject
+  Logger logger;
 
   @Override
   @SneakyThrows
@@ -37,29 +38,27 @@ public class LinearMultiplyAnalyzer extends Analyzer {
             + " and an offset value of "
             + b);
 
-    CSVReader reader = getReader(inputFiles[0]);
-    ICSVWriter writer = getWriter(outputFiles[0]);
+    getReader(inputFiles[0], reader -> {
+      getWriter(outputFiles[0], writer -> {
+        String[] headers = safeReadNext(reader);
+        if (headers == null) {
+          throw new InvalidHeaderException("Failed to read headers from input file: " + inputFiles[0]);
+        }
 
-    String[] headers = reader.readNext();
-    if (headers == null) {
-      throw new InvalidHeaderException("Failed to read headers from input file: " + inputFiles[0]);
-    }
+        int xAxisIndex = this.getColumnIndex(inputColumns[0], headers);
+        int yAxisIndex = this.getColumnIndex(inputColumns[1], headers);
+        writer.writeNext(headers);
 
-    int xAxisIndex = this.getColumnIndex(inputColumns[0], headers);
-    int yAxisIndex = this.getColumnIndex(inputColumns[1], headers);
-    writer.writeNext(headers);
+        String[] dataPoint;
 
-    String[] dataPoint;
-
-    while ((dataPoint = reader.readNext()) != null) {
-      String x = dataPoint[xAxisIndex];
-      double oldY = Double.parseDouble(dataPoint[yAxisIndex]);
-      String newY = Double.toString(linearFunction(oldY));
-      writer.writeNext(new String[] {x, newY});
-    }
-
-    reader.close();
-    writer.close();
+        while ((dataPoint = safeReadNext(reader)) != null) {
+          String x = dataPoint[xAxisIndex];
+          double oldY = Double.parseDouble(dataPoint[yAxisIndex]);
+          String newY = Double.toString(linearFunction(oldY));
+          writer.writeNext(new String[] { x, newY });
+        }
+      });
+    });
   }
 
   private double linearFunction(double x) {
