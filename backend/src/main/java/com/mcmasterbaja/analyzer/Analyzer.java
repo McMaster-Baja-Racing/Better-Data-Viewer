ad58d472a7f9b82ac260a265a5b7cb887e0732a1
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-02-25 14:02:24 -0500

    unsafe analyzers >:(

--- backend/src/main/java/com/mcmasterbaja/analyzer/Analyzer.java
+++ backend/src/main/java/com/mcmasterbaja/analyzer/Analyzer.java
@@ -1,5 +1,14 @@
 package com.mcmasterbaja.analyzer;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
 import com.mcmasterbaja.annotations.OnAnalyzerException;
 import com.mcmasterbaja.exceptions.InvalidColumnException;
 import com.mcmasterbaja.exceptions.InvalidInputFileException;
@@ -10,16 +19,6 @@ import com.opencsv.CSVReaderBuilder;
 import com.opencsv.CSVWriter;
 import com.opencsv.CSVWriterBuilder;
 import com.opencsv.ICSVWriter;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Consumer;
-import lombok.SneakyThrows;
 
 @OnAnalyzerException
 public abstract class Analyzer {
@@ -50,10 +49,11 @@ public abstract class Analyzer {
     Map<String, CSVReader> readersMap = new HashMap<>();
     try {
       for (String filePath : filePaths) {
-        FileReader fileReader = new FileReader(filePath);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        CSVReader reader = new CSVReaderBuilder(bufferedReader).withSkipLines(0).build();
-        readersMap.put(filePath, reader);
+        try (FileReader fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            CSVReader reader = new CSVReaderBuilder(bufferedReader).withSkipLines(0).build()) {
+          readersMap.put(filePath, reader);
+        }
       }
 
       action.accept(readersMap);
@@ -71,7 +71,8 @@ public abstract class Analyzer {
   }
 
   /**
-   * Default behaviour is to use the 0th output file, will need to be overridden in some special
+   * Default behaviour is to use the 0th output file, will need to be overridden
+   * in some special
    * cases
    *
    * @return Filename of the analyzer output
@@ -83,34 +84,16 @@ public abstract class Analyzer {
   public void getWriter(String filePath, Consumer<ICSVWriter> action) {
     try (FileWriter fileWriter = new FileWriter(filePath);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        ICSVWriter writer =
-            new CSVWriterBuilder(bufferedWriter)
-                .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
-                .withQuoteChar(CSVWriter.NO_QUOTE_CHARACTER)
-                .withEscapeChar(CSVWriter.DEFAULT_ESCAPE_CHARACTER)
-                .withLineEnd(CSVWriter.DEFAULT_LINE_END)
-                .build()) {
+        ICSVWriter writer = new CSVWriterBuilder(bufferedWriter)
+            .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
+            .withQuoteChar(CSVWriter.NO_QUOTE_CHARACTER)
+            .withEscapeChar(CSVWriter.DEFAULT_ESCAPE_CHARACTER)
+            .withLineEnd(CSVWriter.DEFAULT_LINE_END)
+            .build()) {
       action.accept(writer);
     } catch (IOException e) {
       throw new InvalidOutputFileException("Failed to write to output file: " + filePath, e);
     }
-  }
-
-  // Safe readers so the interceptor can handle IOExceptions properly, even
-  // from within a lambda function
-  @SneakyThrows
-  protected String[] safeReadNext(CSVReader reader) {
-    return reader.readNext();
-  }
-
-  @SneakyThrows
-  protected List<String[]> safeReadAll(CSVReader reader) {
-    return reader.readAll();
-  }
-
-  @SneakyThrows
-  protected void safeFlush(ICSVWriter writer) {
-    writer.flush();
   }
 
   public int getColumnIndex(String columnName, String[] fileHeaders) {
