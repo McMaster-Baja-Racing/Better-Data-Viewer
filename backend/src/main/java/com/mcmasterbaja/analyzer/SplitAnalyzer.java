6b32573acecbb3581a6243fd6eccd47c5400afe3
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-02-25 11:35:51 -0500

    Refactor SplitAnalyzer

--- backend/src/main/java/com/mcmasterbaja/analyzer/SplitAnalyzer.java
+++ backend/src/main/java/com/mcmasterbaja/analyzer/SplitAnalyzer.java
@@ -1,15 +1,15 @@
 package com.mcmasterbaja.analyzer;
 
+import org.jboss.logging.Logger;
+
 import com.mcmasterbaja.annotations.OnAnalyzerException;
 import com.mcmasterbaja.exceptions.InvalidHeaderException;
 import com.mcmasterbaja.model.AnalyzerParams;
 import com.mcmasterbaja.model.AnalyzerType;
-import com.opencsv.CSVReader;
-import com.opencsv.ICSVWriter;
+
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import lombok.SneakyThrows;
-import org.jboss.logging.Logger;
 
 @Dependent
 @AnalyzerQualifier(AnalyzerType.SPLIT)
@@ -18,7 +18,8 @@ public class SplitAnalyzer extends Analyzer {
   private int start;
   private int end;
 
-  @Inject Logger logger;
+  @Inject
+  Logger logger;
 
   @Override
   @SneakyThrows
@@ -37,28 +38,26 @@ public class SplitAnalyzer extends Analyzer {
             + " and an ending timestamp of "
             + end);
 
-    CSVReader reader = getReader(inputFiles[0]);
-    ICSVWriter writer = getWriter(outputFiles[0]);
+    getReader(inputFiles[0], reader -> {
+      getWriter(outputFiles[0], writer -> {
+        String[] headers = safeReadNext(reader);
+        if (headers == null) {
+          throw new InvalidHeaderException("Failed to read headers from input file: " + inputFiles[0]);
+        }
 
-    String[] headers = reader.readNext();
-    if (headers == null) {
-      throw new InvalidHeaderException("Failed to read headers from input file: " + inputFiles[0]);
-    }
+        int columnIndex = this.getColumnIndex(inputColumns[0], headers);
+        writer.writeNext(headers);
 
-    int columnIndex = this.getColumnIndex(inputColumns[0], headers);
-    writer.writeNext(headers);
+        String[] dataPoint;
 
-    String[] dataPoint;
-
-    while ((dataPoint = reader.readNext()) != null) {
-      if (Integer.parseInt(dataPoint[columnIndex]) >= end) {
-        break;
-      } else if (Integer.parseInt(dataPoint[columnIndex]) >= start) {
-        writer.writeNext(dataPoint);
-      }
-    }
-
-    reader.close();
-    writer.close();
+        while ((dataPoint = safeReadNext(reader)) != null) {
+          if (Integer.parseInt(dataPoint[columnIndex]) >= end) {
+            break;
+          } else if (Integer.parseInt(dataPoint[columnIndex]) >= start) {
+            writer.writeNext(dataPoint);
+          }
+        }
+      });
+    });
   }
 }
