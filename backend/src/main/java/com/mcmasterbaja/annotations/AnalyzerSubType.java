3aea67258cac24ac6bac8c92c87c783e3f727130
Ariel Wolle <kaiarseneau@hotmail.com>
2025-04-26 21:29:00 -0400

    Linting backend
    Co-Authored-By: Kai Arseneau <83799062+gr812b@users.noreply.github.com>

--- backend/src/main/java/com/mcmasterbaja/annotations/AnalyzerSubType.java
+++ backend/src/main/java/com/mcmasterbaja/annotations/AnalyzerSubType.java
@@ -5,5 +5,5 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AnalyzerSubType {
-    String value();
+  String value();
 }
--- backend/src/main/java/com/mcmasterbaja/model/FileInformation.java
+++ backend/src/main/java/com/mcmasterbaja/model/FileInformation.java
@@ -1,7 +1,6 @@
 package com.mcmasterbaja.model;
 
 import java.nio.file.Path;
-
 import lombok.ToString;
 
 // This class represents the data structure that is used to send information about the file through
