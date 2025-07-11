e5b773d9af09346931a14442e4784a577631d590
Kai Arseneau <kaiarseneau@hotmail.com>
2025-07-11 19:18:43 -0400

    Updated import

--- front-end/src/assets/help/index.ts
+++ front-end/src/assets/help/index.ts
@@ -1,12 +1,12 @@
-import accelImage from '../assets/help/accel.png';
-import interpolateImage from '../assets/help/interpolate.png';
-import linearMultiplierImage from '../assets/help/linMult.png';
-import RdpGif from '../assets/help/rdp.gif';
-import RdpImage from '../assets/help/rdp.png';
-import rollAvgImage from '../assets/help/rollAvg.png';
-import savitzkyGolayGif from '../assets/help/sgolay.gif';
-import splitImage from '../assets/help/split.png';
-import placeholderImage from '../assets/help/placeholder.png';
+import accelImage from '@assets/help/accel.png';
+import interpolateImage from '@assets/help/interpolate.png';
+import linearMultiplierImage from '@assets/help/linMult.png';
+import RdpGif from '@assets/help/rdp.gif';
+import RdpImage from '@assets/help/rdp.png';
+import rollAvgImage from '@assets/help/rollAvg.png';
+import savitzkyGolayGif from '@assets/help/sgolay.gif';
+import splitImage from '@assets/help/split.png';
+import placeholderImage from '@assets/help/placeholder.png';
 
 export {
   accelImage,
