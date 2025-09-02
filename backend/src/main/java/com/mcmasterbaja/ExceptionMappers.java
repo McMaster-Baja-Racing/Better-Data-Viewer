06efaffb22bf9bb0354ce8e13d6232e21d266aef
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-09-02 15:25:49 -0400

    Refactor logger injection and format error response

--- backend/src/main/java/com/mcmasterbaja/ExceptionMappers.java
+++ backend/src/main/java/com/mcmasterbaja/ExceptionMappers.java
@@ -12,28 +12,21 @@ import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 // Exceptions are mapped in priority of most specific first
 public class ExceptionMappers {
 
-  @Inject
-  Logger logger;
+  @Inject Logger logger;
 
   private Response mapBajaException(BajaException e) {
     String errorId = UUID.randomUUID().toString();
-    logger.error("errorId[{}]", errorId, e);
-
-    ErrorResponse errorResponse = e.toErrorResponse();
-
-    return Response.status(500).entity(errorResponse).type(MediaType.APPLICATION_JSON).build();
-  }
-
-  private Response mapSystemError(Throwable e, String errorCode, String message) {
+	@@ -27,13 +28,12 @@ private Response mapSystemError(Throwable e, String errorCode, String message) {
     String errorId = UUID.randomUUID().toString();
     logger.error("errorId[{}]", errorId, e);
 
-    ErrorResponse errorResponse = new ErrorResponse(
-        errorId,
-        e.getStackTrace()[0].getClassName() + "." + e.getStackTrace()[0].getMethodName(),
-        message,
-        errorCode,
-        e.getMessage());
+    ErrorResponse errorResponse =
+        new ErrorResponse(
+            errorId,
+            e.getStackTrace()[0].getClassName() + "." + e.getStackTrace()[0].getMethodName(),
+            message,
+            errorCode,
+            e.getMessage());
 
     return Response.status(500).entity(errorResponse).type(MediaType.APPLICATION_JSON).build();
   }
