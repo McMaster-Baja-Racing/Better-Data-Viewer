916be6e62b74f06c7a60c7c598f05377a2c9c22e
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-09-03 11:24:38 -0400

    create graph tests fixed

--- e2e/testers/upload.ts
+++ e2e/testers/upload.ts
@@ -73,7 +73,7 @@ export class UploadTester extends Tester {
   }
 
   async verifyFilesUploaded() {
-    await expect(this.page.getByRole('heading', { name: 'Files uploaded successfully' })).toBeVisible();
+    await expect(this.page.getByText('Files uploaded successfully')).toBeVisible();
   }
 
   async verifyNoFileSelectedAlert() {
