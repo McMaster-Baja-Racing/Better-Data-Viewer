fb168da71534547f341e89e3ce5b93a0bcdfafcb
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-09-05 12:57:00 -0400

    273 fix e2e testing (#274)
    * upload tests fixed
    
    * create graph tests fixed
    
    * Fix missing newline at end of package-lock.json
    
    * Update package-lock.json
    
    why did it even touch this who knows
    
    * Delete backend/hs_err_pid32532.log
    
    * Delete backend/hs_err_pid4252.log
    
    * Delete backend/hs_err_pid6764.log
    
    * revert a lot of changes and try again w just the /old removed
    
    * attempt to address crash due to undefined data type
    
    * undo change to lint yaml
    
    Simplified backend server startup command in lint workflow.
    
    * seems to be calling an old function

--- e2e/testers/graph.ts
+++ e2e/testers/graph.ts
@@ -2,44 +2,31 @@ import { Page, expect } from '@playwright/test';
 import { Tester } from './tester';
 
 export class GraphTester extends Tester {
-  frontendFilePath: string;
-
   constructor(page: Page, binFileName: string) {
     super(page);
-    this.frontendFilePath = `${binFileName}/BATT VOLT`;
   }
 
-  async testCreateGraph() {
-    await this.openGraphForm();
-    await this.pressNextButton();
-    await this.selectFile(this.frontendFilePath);
-    await this.pressNextButton();
-    await this.submitGraph();
-    await this.verifyGraphCreated();
+  async selectPreset(presetButtonName: string) {
+    await this.page.getByRole('button', { name: presetButtonName }).click();
   }
 
-  // Basic functions
-
-  async openGraphForm() {
-    await this.page.getByRole('button', { name: 'Create Graph' }).click();
-  }
-
-  async pressNextButton() {
-    await this.page.getByRole('button', { name: 'Next' }).click();
-  }
-
-  async selectFile(filePath: string) {
-    const splitPath = filePath.split('/');
-    for (const folder of splitPath) {
-      await this.page.getByRole('cell', { name: folder }).click();
-    }
+  async selectExistingFile(fileName: string) {
+    await this.page.getByRole('cell', { name: fileName }).click();
   }
 
   async submitGraph() {
-    await this.page.getByRole('button', { name: 'Submit' }).click();
+    await this.page.getByRole('button', { name: 'Submit right arrow' }).click();
   }
 
-  async verifyGraphCreated() {
-    await expect(this.page.getByText('Graph Created')).toBeVisible();
+  async verifyDashboardCreated() {
+    // todo better way to verify that a graph was actually made
+    await expect(this.page.getByText('Dashboard', { exact: true })).toBeVisible();
+  }
+
+  async testCreateGraphWithPreset(presetButtonName: string, fileName: string) {
+    await this.selectPreset(presetButtonName);
+    await this.selectExistingFile(fileName);
+    await this.submitGraph();
+    await this.verifyDashboardCreated();
   }
 }
