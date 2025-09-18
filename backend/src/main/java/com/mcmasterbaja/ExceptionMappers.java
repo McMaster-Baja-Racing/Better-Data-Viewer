28e291875946a21e0f65d59c95a7c7da64520802
vjhawar12 <vedantj1203@gmail.com>
2025-09-18 16:37:29 -0400

    imported IOException in ExceptionMappers

--- backend/src/main/java/com/mcmasterbaja/ExceptionMappers.java
+++ backend/src/main/java/com/mcmasterbaja/ExceptionMappers.java
@@ -14,6 +14,7 @@ import jakarta.ws.rs.core.Response;
 import java.util.UUID;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+import java.io.IOException;
 
 // Exceptions are mapped in priority of most specific first
 public class ExceptionMappers {
