c4680bdfe0dafa095874b49f3cd5759c2d36b067
hans-hc <hansencheng635@gmail.com>
2026-01-19 20:55:04 -0500

    Home Page minor tweaks, Preset Card modal fix
    Some more tweaks on the home page, and made the preset card modal when clicked from the home screen responsive. Removed upload file option from preset card as per instructed

--- front-end/src/components/composite/presetFilesModal/PresetFilesModal.tsx
+++ front-end/src/components/composite/presetFilesModal/PresetFilesModal.tsx
@@ -1,7 +1,6 @@
 import styles from './PresetFilesModal.module.scss';
 import { BaseModal } from '@components/ui/baseModal/BaseModal';
 import { FileTable } from '@components/ui/fileTable/FileTable';
-import { UploadForm } from '@components/ui/uploadForm/UploadForm';
 import { Button } from '@components/ui/button/Button';
 import { useState, useEffect } from 'react';
 import { rightArrowIcon } from '@assets/icons';
@@ -22,8 +21,7 @@ interface PresetFilesModalProps {
 export const PresetFilesModal = ({ onClose, isOpen, onSubmit, currentSources }: PresetFilesModalProps) => {
   // TODO: Adjust based on preset
   const [selectedFiles, setSelectedFiles] = useState<FileInformation[]>([]);
-  const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
-  const { data: files, refetch } = useFiles();
+  const { data: files } = useFiles();
 
   // Pre-select files based on currentSources prop
   useEffect(() => {
@@ -37,47 +35,26 @@ export const PresetFilesModal = ({ onClose, isOpen, onSubmit, currentSources }: 
   }, [currentSources, files]);
 
   const handleSubmit = async () => {
-    if (selectedFiles.length === 0 && uploadedFiles.length === 0) {
-      showWarningToast('Oops!', 'Please select or upload at least one file');
+    if (selectedFiles.length === 0) {
+      showWarningToast('Oops!', 'Please select at least one file');
       return;
     }
 
-    const uploadPromises = uploadedFiles.map((file) => ApiUtil.uploadFile(file));
-    await Promise.all(uploadPromises);
-
     const fileKeys = selectedFiles.map((file) => file.key);
-    
-    const uploadedFolderNames = uploadedFiles.map((file) => {
-      return file.name.substring(0, file.name.lastIndexOf('.'));
-    });
-    
-    fileKeys.push(...uploadedFolderNames);
-
     onSubmit(fileKeys);
     onClose();
-    refetch(); // Refetch files after upload
   };
 
   return (
     <BaseModal isOpen={isOpen} onClose={onClose}>
       <div className={styles.presetFilesModal}>
-
-        <div className={styles.formWrapper}>
-          <div className={styles.uploadForm}>
-            <h2 className={styles.title}>Upload your files</h2>
-            <UploadForm files={uploadedFiles} setFiles={setUploadedFiles} accept={'.bin'}/>
-          </div>
-
-          <div className={styles.selectFiles}>
-            <h2 className={styles.title}>...Or select existing ones</h2>
-            <div className={styles.tableWrapper}>
-              <FileTable 
-                files={getFolders(files || [])} 
-                selectedFiles={selectedFiles} 
-                setSelectedFiles={setSelectedFiles} 
-              />
-            </div>
-          </div>
+        <h2 className={styles.title}>Select a file</h2>
+        <div className={styles.tableWrapper}>
+          <FileTable 
+            files={getFolders(files || [])} 
+            selectedFiles={selectedFiles} 
+            setSelectedFiles={setSelectedFiles} 
+          />
         </div>
 
         <Button 
